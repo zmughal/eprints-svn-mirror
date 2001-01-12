92cb597089ebcf59721c716314d27873ebe71330
cjg <cjg@9491667e-5006-0410-a446-efbe8990b998>
2001-01-12 15:08:17 +0000

    Initial attempts to remove GNU-only commands.

---
+++ release/install-scripts/findfiles.pl
@@ -0,0 +1,23 @@
+
+# This perl script finds all the files, and only the files in a given directory.
+#
+# It has the same functionality as gnu find with the options -maxdepth 1 -type f
+# but is (hopefully) more portable.
+#
+# Christopher Gutteridge (12/01/2001)
+
+if (scalar @ARGV!=1) {
+	print STDERR "Usage: perl findfiles.pl <directory>\n\n";
+	exit 1;
+}
+
+$dir=$ARGV[0];
+opendir(D,$dir); 
+while( $f=readdir(D) ) 
+{ 
+	if ( -f "$dir/$f" ) 
+	{
+		print "$f\n"; 
+	}
+} 
+closedir(D);
