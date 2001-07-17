b5deb1a3803e31c7d527179fe003652644357593
moj199 <moj199@9491667e-5006-0410-a446-efbe8990b998>
2001-07-17 10:53:38 +0000

    Added installer.

---
+++ release_ep2/install-eprints.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+print <<INTRO;
+EPrints 2 Installer
+-------------------
+
+This installer creates the necessary
+directories and files for you to begin
+configuring EPrints 2.
+
+INTRO
+
