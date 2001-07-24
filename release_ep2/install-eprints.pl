ecebc21f649df6059b4bba0c54bc61f99f1e776b
moj199 <moj199@9491667e-5006-0410-a446-efbe8990b998>
2001-07-24 11:45:48 +0000

    SystemSettings generation nearly there. Added VERSION file handling (to make release generation a little easier). File detection now works nicely, and there's a pretty mini-progress bar thing when installation happens.

--- release_ep2/install-eprints.pl
+++ release_ep2/install-eprints.pl
@@ -11,7 +11,7 @@ you to begin configuring EPrints 2.
 
 INTRO
 
-print "Checking user...";
+print "Checking user ... ";
 if (0) #($<!=0) -- removed while testing.
 {
 	print "Failed!\n";
@@ -20,6 +20,49 @@ if (0) #($<!=0) -- removed while testing.
 }
 else { print "OK.\n\n"; }
 
+# Grab version number from VERSION file.
+open(VERSIONIN, "VERSION") or die "No VERSION file - invalid distribution?";
+my $version = <VERSIONIN>;
+if (!defined($version)) { die "Undefined version number."; }
+chomp($version);
+my $version_desc = <VERSIONIN>;
+if (!defined($version_desc)) { die "Undefined version descriptor."; }
+chomp($version_desc);
+close(VERSIONIN);
+
+
+my @paths = ("/bin/", "/usr/bin/", "/usr/local/bin/", "/usr/sbin/");
+my %exesettings = ();
+my %systemsettings = ();
+
+# Set up some default settings.
+my %invocsettings = (
+	ZIP     => "\$(EXE_ZIP) 1>/dev/null 2>\&1 -qq -o -d \$(DIR) \$(ARC)",
+	TARGZ   => "\$(EXE_GUNZIP) -c < \$(ARC) 2>/dev/null | \$(EXE_TAR) xf - -C \$(DIR) >/dev/null 2>\&1",
+	WGET    => "\$(EXE_WGET)  -r -L -q -m -nH -np --execute=\"robots=off\" --cut-dirs=\$(CUTDIRS) \$(URL)",
+	SENDMAIL => "\$(EXE_SENDMAIL) -oi -t -odb --"
+);
+
+my %archiveexts = (
+	"ZIP"    =>  ".zip",
+	"TARGZ"  =>  ".tar.gz"
+);
+
+
+$systemsettings{"invocation"} = \%invocsettings;
+$systemsettings{"archive_extensions"} = \%archiveexts;
+$systemsettings{"archives"} = ("ZIP", "TARGZ");
+$systemsettings{"version"} = "2.0.a.2001-09-04";
+$systemsettings{"version_desc"} = "EPrints 2.0 Alpha (Nightly Build 2001-09-04)";
+$exesettings{"unzip"} 	= detect("unzip", @paths);
+$exesettings{"wget"} 	= detect("wget", @paths);
+$exesettings{"sendmail"} = detect("sendmail", @paths);
+$exesettings{"gunzip"} 	= detect("gunzip", @paths);
+$exesettings{"tar"} 	= detect("tar", @paths);
+
+$systemsettings{"executable"} = \%exesettings;
+$systemsettings{"version"} = $version;
+$systemsettings{"version_desc"} = $version_desc;
 print <<DIR;
 EPrints installs by default to the /opt/eprints directory. If you
 would like to install to a different directory, please specify it
@@ -55,6 +98,8 @@ DIRWARN
 		$dirokay = 1;
 	}
 }
+$systemsettings{"base_path"} = $dir;
+
 print <<USER;
 EPrints must be run as a non-root user (typically 'eprints').
 Please specify the user that you wish to use, or press enter to use
@@ -83,7 +128,7 @@ GROUP
 
 	if (!$gexists)
 	{
-		print "Creating group...";
+		print "Creating group ... ";
 		if (system("/usr/sbin/groupadd $group")==0)
 		{
 			print "OK.\n\n";
@@ -96,7 +141,7 @@ GROUP
 		}
 	}
 
-	print "Creating user...";
+	print "Creating user ... ";
 	if (system("/usr/sbin/useradd -s /bin/false -d $dir -g $group $user")==0)
 	{
 		print "OK.\n\n";
@@ -109,7 +154,7 @@ GROUP
 	}
 }
 
-print "\nMaking directory...";
+print "\nMaking directory ... ";
 if (!-d $dir && !mkdir($dir))
 {
 	print "Failed!\n";
@@ -119,7 +164,7 @@ if (!-d $dir && !mkdir($dir))
 else
 { print "OK.\n\n"; }
 
-print "Installing files...\n";
+print "Installing files : [";
 
 my(undef,undef,$uid,$gid) = getpwnam($user);
 my @executable_dirs = ("bin", "cgi");
@@ -128,12 +173,20 @@ my @normal_dirs = ("defaultcfg", "cfg", "perl_lib", "phrases");
 foreach(@executable_dirs)
 {
 	install($_, 0755, $uid, $gid, $dir);	
+	print "|";
 }
 
 foreach(@normal_dirs)
 {
 	install($_, 0644, $uid, $gid, $dir);
+	print "|";
 }
+
+print "]\n\n";
+
+use Data::Dumper;
+$Data::Dumper::Indent = 3;
+print Dumper(\%systemsettings);
 
 sub install
 {
@@ -172,6 +225,7 @@ sub install
 	}
 	foreach(@dirs)
 	{
+		print "|";
 		install("$dir/".$_, $perms, $user, $group, $dest);
 	}
 }
@@ -205,4 +259,53 @@ sub get_string
 		if ($response eq "") { $response = $default; }
 	} while ($response !~ m/^$regexp$/);
 	return $response;
+}
+
+sub find_file
+{
+	my($findme, @paths) = @_;
+	my @searchpaths = split(/:+/, $ENV{"PATH"});
+	push @searchpaths, @paths;
+	my @found = ();
+	my $curritem;
+	foreach my $path (@searchpaths)
+	{
+		$path =~ s/(.+)\/$/$1/;
+		opendir(DIR, $path) or next;
+		while ($curritem = readdir(DIR))
+		{
+			next if ($curritem =~ /^\.[\.]?$/);
+			if ($curritem eq $findme)
+			{
+				push @found, "$path/$curritem";
+			}
+		}
+		closedir(DIR);
+	}
+	return @found;
+}
+
+sub detect
+{
+	my($todetect, @path) = @_;
+	print "Detecting $todetect ... ";
+	my @paths = find_file($todetect, @path);
+	my $found = $paths[0];
+
+	if (defined($found))
+	{
+		print "Found [".$found."]\n";
+	}
+	else
+	{
+		print "Not found!\n\n";
+		while (!defined($found))
+		{
+			print "Couldn't locate $todetect. Please specify the path to this file.\n";
+			my $path = get_string("[a-zA-Z0-9_\/]+", "Path", "");
+			if (!-e $path) { $found = undef; }
+			else { $found = $path; }
+		}
+	}
+	return $found
 }
