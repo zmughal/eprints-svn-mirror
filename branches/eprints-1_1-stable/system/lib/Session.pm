aec6ce504ed7c24ab44944c01e1dccac01607625
cjg <cjg@9491667e-5006-0410-a446-efbe8990b998>
2001-08-29 13:58:33 +0000

    - update to version

--- branches/eprints-1_1-stable/system/lib/Session.pm
+++ branches/eprints-1_1-stable/system/lib/Session.pm
@@ -33,7 +33,7 @@ use strict;
 
 
 # GLOBAL SITE REVISION NUMBER
-$EPrints::Session::eprints_software_version = "Version 1.1.2 (20/02/2001)";
+$EPrints::Session::eprints_software_version = "Version 1.1.2 (29/08/2001)";
 
 
 ######################################################################
