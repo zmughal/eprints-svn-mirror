1fab3d32bfda78e086be29e13a88f75cce886f3c
moj199 <moj199@9491667e-5006-0410-a446-efbe8990b998>
2002-02-08 16:57:46 +0000

    Lots of updates: - Now generates a SIGNATURE file of MD5 hashes to check for altered files. - Updated detect sub to allow optional files - Added latex/dvipis/convert to optional files. - Now ignores old version id. - Checks to see if we're 2.0/2.0a, etc. - Now has recovery mode (or 'upgrade to same version') - Stores version history - Added df checking - Added post_install method for extra functionality. - Split installer up into two subs - added install_file.

--- trunk/release_ep2/install-eprints.pl
+++ trunk/release_ep2/install-eprints.pl
@@ -16,7 +16,7 @@
 
 use strict;
 use Cwd;
-
+use Digest::MD5;
 my $forced = 0;
 if (defined($ARGV[0]))
 {
@@ -40,10 +40,9 @@ my $non_root_warning = <<WARNING;
 
 Running as non-root will cause some stages to be skipped:
 - No user/group creation will be attempted
-- The file owner flags will not be specificly set
+- The file owner flags will not be specifically set
 
 WARNING
-
 
 if ($forced)
 {
@@ -75,6 +74,7 @@ close(VERSIONIN);
 
 
 my @paths = ("/bin/", "/usr/bin/", "/usr/local/bin/", "/usr/sbin/");
+my @signatures = (); # MD5 signature store
 my %exesettings = ();
 my %systemsettings = ();
 
@@ -83,7 +83,10 @@ my %invocsettings = (
 	zip     => '$(zip) 1>/dev/null 2>&1 -qq -o -d $(DIR) $(ARC)',
 	targz   => '$(gunzip) -c < $(ARC) 2>/dev/null | $(tar) xf - -C $(DIR) >/dev/null 2>&1',
 	wget    => '$(wget)  -r -L -q -m -nH -np --execute="robots=off" --cut-dirs=$(CUTDIRS) $(URL)',
-	sendmail => '$(sendmail) -oi -t -odb --'
+	sendmail => '$(sendmail) -oi -t -odb --',
+	latex => '$(latex) \'$(SOURCE)\'',
+	dvips => '$(dvips) \'$(SOURCE)\' -o \'$(TARGET)\'',
+	convert_crop_white => '$(convert) -crop 0x0 -bordercolor white -border 4x4 \'$(SOURCE)\' \'$(TARGET)\'' 
 );
 
 my %archiveexts = (
@@ -91,14 +94,19 @@ my %archiveexts = (
 	"targz"  =>  ".tar.gz"
 );
 
-
-$exesettings{"unzip"} 	= detect("unzip", @paths);
-$exesettings{"wget"} 	= detect("wget", @paths);
-$exesettings{"sendmail"} = detect("sendmail", @paths);
-$exesettings{"gunzip"} 	= detect("gunzip", @paths);
-$exesettings{"tar"} 	= detect("tar", @paths);
-my $useradd = detect("useradd", @paths);
-my $groupadd = detect("groupadd", @paths);
+print "Detecting required binaries:\n";
+$exesettings{"unzip"} 	= detect("unzip", 1, @paths);
+$exesettings{"wget"} 	= detect("wget", 1, @paths);
+$exesettings{"sendmail"} = detect("sendmail", 1, @paths);
+$exesettings{"gunzip"} 	= detect("gunzip", 1, @paths);
+$exesettings{"tar"} 	= detect("tar", 1, @paths);
+my $useradd = detect("useradd", 1, @paths);
+my $groupadd = detect("groupadd", 1, @paths);
+print "\nDetecting optional binaries:\n";
+$exesettings{"latex"}	= detect("latex", 0, @paths);
+$exesettings{"dvips"}	= detect("dvips", 0, @paths);
+$exesettings{"convert"}	= detect("convert", 0, @paths);
+my $h2ph = detect("h2ph", 0, @paths);
 
 $systemsettings{"invocation"} = \%invocsettings;
 $systemsettings{"archive_extensions"} = \%archiveexts;
@@ -117,8 +125,8 @@ DIR
 my $dirokay = 0;
 my $dir = "";
 my $upgrade = 0;
-my $orig_version_id = "";
-my $orig_version = "";
+my $newv;
+my $oldv;
 while (!$dirokay)
 {
 	$dir = get_string('[\/a-zA-Z0-9_]+', "Directory", "/opt/eprints2");
@@ -128,13 +136,53 @@ while (!$dirokay)
 		if (-e "$dir/perl_lib/EPrints/SystemSettings.pm")
 		{
 			require "$dir/perl_lib/EPrints/SystemSettings.pm" or die("Unable to detect SystemSettings module: Corrupt prevous install?");
-			my $old_version_id = $EPrints::SystemSettings::conf{"version_id"};
+			#my $old_version_id = $EPrints::SystemSettings::conf{"version_id"};
+			$systemsettings{"user"} = $EPrints::SystemSettings::conf->{"user"};
+			$systemsettings{"group"} = $EPrints::SystemSettings::conf->{"group"};
+			$oldv = $EPrints::SystemSettings::conf->{"version_id"};
+			$newv = $systemsettings{"version_id"};
+			# Mild cheat to make sure 2.0.a is before 2.0, etc.
+			$oldv =~ s/^2\.0\.a/0\.0\.0/;
+			$newv =~ s/^2\.0\.a/0\.0\.0/;
+			# Gently warn the user if Bad Things could happen.
+			if ($oldv =~ /-[0-9]{4}-[0-9]{2}-[0-9]{2}$/)
+			{
+				# Upgrading CVS->CVS
+				if ($newv =~ /-[0-9]{4}-[0-9]{2}-[0-9]{2}$/)
+				{
+					print <<WARNING_N2N;
+*** Warning ***
+You are upgrading from a nightly release to another nightly release. This
+may cause weird things to happen!
+*** Warning ***
+				
+WARNING_N2N
+				}
+				# Upgrading CVS->Stable
+				else
+				{
+					print <<WARNING_N2S;
+*** Warning ***
+You are upgrading from a nightly release to a stable release. This
+may cause weird things to happen!
+*** Warning ***
 
-			$systemsettings{"orig_version_id"} = $EPrints::SystemSettings::conf->{"orig_version_id"};
-			$systemsettings{"orig_version"} = $EPrints::SystemSettings::conf->{"orig_version"};
-			my $origv = $systemsettings{"orig_version_id"};
-			my $newv = $systemsettings{"version_id"};	
-			if (defined $origv && $origv gt $newv && 0 ) # buggy so not doing it! cjg
+WARNING_N2S
+				}
+			}
+			# Upgrading Stable->CVS
+			elsif ($newv =~ /-[0-9]{4}-[0-9]{2}-[0-9]{2}$/)
+			{
+				print <<WARNING_S2N;
+*** Warning ***
+You are upgrading from a stable release to a nightly release. This
+may cause weird things to happen!
+*** Warning *** 
+
+WARNING_S2N
+			}
+
+			if (defined $oldv && $oldv gt $newv) # buggy so not doing it! cjg
 			{
 				print <<DOWNGRADE;
 You already have a version of EPrints installed in this directory and it
@@ -144,10 +192,19 @@ Please obtain the latest version of EPrints and try again.
 DOWNGRADE
 				exit 1;
 			}
-			elsif (defined $origv && $origv eq $newv)
+			elsif (defined $oldv && $oldv eq $newv)
 			{
-				print "You already have this version installed.\n";
-				exit 1;
+				print <<RECOVER;
+You already have this version installed here. Would you like to recover the
+installation (replace core binaries, documentation, etc)?
+
+RECOVER
+				if (get_yesno("Recover?", "n") eq "y")
+				{
+					$dirokay = 1;
+					$upgrade = 1;
+				}
+				else { $dirokay = 0; }
 			}
 			else
 			{
@@ -182,6 +239,13 @@ DIRWARN
 	}
 }
 $systemsettings{"base_path"} = $dir;
+my @foo = ();
+if (defined @{$EPrints::SystemSettings::conf->{"version_history"}})
+{
+	@foo =  @{$EPrints::SystemSettings::conf->{"version_history"}};
+}
+push @foo, $newv;
+$systemsettings{"version_history"} = \@foo;
 
 if (!$upgrade) { full_install(); }
 else { upgrade(); }
@@ -291,8 +355,6 @@ GROUP
 	}
 	$systemsettings{"user"} = $user;
 	$systemsettings{"group"} = $group;	
-	$systemsettings{"orig_version_id"} = $systemsettings{"version_id"}; # First time install
-	$systemsettings{"orig_version"} = $systemsettings{"version"};
 	my(undef,undef,$uid,$gid) = getpwnam($user);
 	print "\nMaking directory ... ";
 	if (!-d $dir && !mkdir($dir, 0755))
@@ -323,6 +385,8 @@ GROUP
 	}
 	
 	print "]\n\n";
+	
+	post_install($dir);
 
 	print <<HOORAY;
 
@@ -361,15 +425,160 @@ Hooray! Your EPrints2 installation was successful!
 HOORAY
 }
 
+sub detect_df 
+{
+
+	my $dir = "/";
+	my ($fmt, $res);
+
+	# try with statvfs..
+	eval 
+	{  
+		{
+			package main;
+			require "sys/syscall.ph";
+		}
+		$fmt = "\0" x 512;
+		$res = syscall (&main::SYS_statvfs, $dir, $fmt) ;
+		$res == 0;
+	}
+	# try with statfs..
+	|| eval 
+	{ 
+		{
+			package main;
+			require "sys/syscall.ph";
+		}	
+		$fmt = "\0" x 512;
+		$res = syscall (&main::SYS_statfs, $dir, $fmt);
+		$res == 0;
+	}
+}
+
+
+
+# Post installation bits
+
+sub post_install
+{
+	my($dir) = @_;
+	print "All files installed.\n";
+	print "Detecting df...";
+	my $df_available = detect_df();
+	if (!$df_available)
+	{
+		print " Not Found.\n";
+		print <<END;
+
+df is currently unavailable on your server. To enable it, the installer
+can run 'h2ph * */*' in your /usr/include directory.
+END
+#	my $doh2ph = get_yesno("Run h2ph", "n");
+	}
+#	else
+#	{
+#		print " Detected.\n";  	
+#	}
+
+	# Dump out signature file
+	open (SIGOUT, ">$dir/SIGNATURES");
+	foreach(@signatures)
+	{
+		print SIGOUT $_;
+	}
+	close(SIGOUT);
+}
+
+# Calculate the MD5 digest of a file.
+
+sub get_digest
+{
+	my($file) = @_;
+	open(FILE, $file) or die "Can't open '$file': $!";
+	my $outdigest = new Digest::MD5;
+	foreach(<FILE>)
+	{
+		$outdigest->add($_);
+	}
+	close(FILE);
+	return $outdigest->hexdigest;
+}
+
+# Install $file from directory $dir into $dest. The permissions
+# from $perms are set, and the group and user are set from $group
+# and $user. The %hash is used for MD5 comparisions - currently
+# not doing anything active, but checking is enabled.
+
+sub install_file
+{
+	my($file, $dir, $perms, $user, $group, $dest, %hash) = @_;
+	my $currdir = getcwd();
+	return unless (-e "$currdir/$dir/$file");
+	my $outdigest = Digest::MD5->new;
+	my @linesout = ();	
+	my $currline = "";
+	open(INFILE, "$currdir/$dir/$file");
+	if ($dir eq "bin")
+	{
+		if ($file eq "startup.pl")
+		{
+			$currline = "use lib '".$dest."/perl_lib';\n";
+			$outdigest->add($currline);
+			push @linesout, $currline;
+		}
+		else
+		{
+			$currline = "#!/usr/bin/perl -w -I".$dest."/perl_lib\n";
+			$outdigest->add($currline);
+			push @linesout, $currline;
+		}
+		my $skipme = <INFILE>;	
+	}
+	while(my $line=<INFILE>)
+	{
+		chomp $line; 
+		$currline = "$line\n";
+		$outdigest->add($currline);
+		push @linesout, $currline;
+	}		
+	close(INFILE);
+	push @signatures, "$dir/$file ".$outdigest->hexdigest."\n";
+
+	# Note that this can't handle the bin dir yet - as that gets updated
+	# prior to writing. Could be fixed by putting output files to a temp
+	# dir, then comparing, then moving to the correct location - but
+	# that's a bit like overkill.
+	
+	if ($hash{"$dir/$file"} && -e "$dest/$dir/$file")
+	{
+		my $repl_digest = get_digest("$dest/$dir/$file");
+		if ($repl_digest ne $hash{"$dir/$file"})
+		{
+			print "[$repl_digest][".$hash{"$dir/$file"}."]$dir/$file\n";
+			# mikej Backup altered file code goes here
+		}
+	}
+
+	open(OUTFILE, ">$dest/$dir/$file") or die "Can't write to $dest/$dir/$file : $!";	foreach(@linesout)
+	{
+		print OUTFILE $_;
+	}	
+	close(OUTFILE);
+
+	chmod($perms, "$dest/$dir/$file") or die "Unable to chmod $dest/$dir/$file : $!";
+	if (!$forced) { chown($user, $group, "$dest/$dir/$file") or die "Unable to chown $dest/$dir/$file : $!"; }
+}
+
 sub install
 {
-	my($dir, $perms, $user, $group, $dest) = @_;
+	my($dir, $perms, $user, $group, $dest, %hash) = @_;
 	opendir(INDIR, $dir) or die("Unable to install directory: $dir. $!");
 	my @dirs = ();
 	my @files = ();
 	my $currdir = getcwd();
 	mkdir("$dest/$dir", 0755);
 	if (!$forced) {chown($user, $group, "$dest/$dir") or die "Unable to chown $dest/$dir/$_ : $!"; }
+
 	while(my $item = readdir(INDIR))
 	{
 		if ($item =~ /^\./ && $item ne ".htaccess" ) { next; }
@@ -379,32 +588,12 @@ sub install
 	closedir(INDIR);
 	foreach(@files)
 	{
-		open(INFILE, "$currdir/$dir/$_");
-		open(OUTFILE, ">$dest/$dir/$_") or die "Can't write to $dest/$dir/$_ : $!";
-		if ($dir eq "bin")
-		{
-			if ($_ eq "startup.pl")
-			{
-				print OUTFILE "use lib '".$dest."/perl_lib';\n";
-			}
-			else
-			{
-				print OUTFILE "#!/usr/bin/perl -w -I".$dest."/perl_lib\n";
-			}
-			my $skipme = <INFILE>;	
-		}
-		while(my $line=<INFILE>)
-		{
-			print OUTFILE $line;
-		}		
-		close(OUTFILE);
-		close(INFILE);
-		chmod($perms, "$dest/$dir/$_") or die "Unable to chmod $dest/$dir/$_ : $!";
-		if (!$forced) { chown($user, $group, "$dest/$dir/$_") or die "Unable to chown $dest/$dir/$_ : $!"; }
+		install_file($_, $dir, $perms, $user, $group, $dest, %hash);
 	}
 	foreach(@dirs)
 	{
 		print "|";
+		$|=1;
 		install("$dir/".$_, $perms, $user, $group, $dest);
 	}
 }
@@ -415,6 +604,23 @@ sub upgrade
 	my $dir  = $EPrints::SystemSettings::conf->{"base_path"};
 
 	my(undef,undef,$uid,$gid) = getpwnam($user);
+	my $checkMD5s = 0;
+	my $digest;
+	my $file;
+	my %MD5Hash = ();
+	# If we have a digest file, read the digests into a hash
+	if (-e "$dir/SIGNATURES")
+	{
+		print "Loading SIGNATURES...\n";
+		open(MD5IN, "$dir/SIGNATURES");
+		foreach(<MD5IN>)
+		{
+			chomp;
+			($file, $digest) = split(" ", $_);
+			$MD5Hash{$file} = $digest;
+		}
+		close(MD5IN);
+	}
 	
 	print <<WARN;
 Warning: This will overwrite phrase files, program files, and documentation.
@@ -426,25 +632,36 @@ WARN
 	print "Upgrading files : [";
 
         my @executable_dirs = ("bin", "cgi");
-        my @normal_dirs = ("sys", "docs", "perl_lib", "phrases");
-
+	my @normal_dirs = ("defaultcfg", "cfg", "docs", "perl_lib");
+	my @base_files = ("VERSION", "CHANGELOG", "BUGLIST", "COPYING", "ROADMAP"); 
         foreach(@executable_dirs)
         {
-                install($_, 0755, $uid, $gid, $dir);
+                install($_, 0755, $uid, $gid, $dir, %MD5Hash);
                 print "|";
+		$|=1;
         }
 
         foreach(@normal_dirs)
         {
-                install($_, 0644, $uid, $gid, $dir);
+                install($_, 0644, $uid, $gid, $dir, %MD5Hash);
                 print "|";
+		$|=1;
         }
 
+	foreach(@base_files)
+	{
+		install_file($_, "", 0644, $uid, $gid, $dir, %MD5Hash);
+	}
+
         print "]\n\n";
+
+	post_install();
+
 	print <<HOORAY;
 =====
 =
-= Update successful. You should now restart your webserver and hope for the best.
+= Update successful. You should now re-run bin/generate_apacheconf, and restart
+= your web server.
 =
 =====
 
@@ -508,7 +725,7 @@ sub find_file
 
 sub detect
 {
-	my($todetect, @path) = @_;
+	my($todetect, $compulsory, @path) = @_;
 	print "Detecting $todetect ... ";
 	my @paths = find_file($todetect, @path);
 	my $found = $paths[0];
@@ -517,7 +734,7 @@ sub detect
 	{
 		print "Found [".$found."]\n";
 	}
-	else
+	elsif($compulsory)
 	{
 		print "Not found!\n\n";
 		while (!defined($found))
@@ -527,6 +744,11 @@ sub detect
 			if (!-e $path) { $found = undef; }
 			else { $found = $path; }
 		}
+	}
+	elsif(!$compulsory)
+	{
+		print "Not found\n\n";
+		$found = undef;
 	}
 	return $found;
 }
