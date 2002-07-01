06a3c48adb256ad109b279700b0f5699d184b425
cjg <cjg@9491667e-5006-0410-a446-efbe8990b998>
2002-07-01 15:32:30 +0000

    Gone back to using configure! Almost works expcept generated install.pl file is not executable!

--- release_ep2/makepackage.pl
+++ release_ep2/makepackage.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl -w
 
 use Cwd;
+use strict;
 
-%codenames = (
+my %codenames = (
 	"eprints2-alpha-1" => "anchovy",
 	"eprints2-alpha-2" => "pepperoni",
 	"eprints2-pre-1"   => "fishfinger",
@@ -14,38 +15,40 @@ use Cwd;
 	"eprints2-pre-7"   => "limepickle",
 	"eprints2-2-0"   => "olive",
 	"eprints-2-0-1pre1"   => "mangogoo",
-	"eprints2-0-1"   => "tuna"
+	"eprints2-0-1"   => "tuna",
+	"eprints2-1"   => "pineapple"
 );
-%ids = (
-	"latest"           => "2.0.1",
+
+my %ids = (
 	"eprints2-pre-6"   => "2.0.pre-6",
 	"eprints2-2-0"     => "2.0",
 	"eprints-2-0-1pre1"     => "2.0.1.pre-1",
-	"eprints2-0-1"     => "2.0.1"
+	"eprints2-0-1"     => "2.0.1",
+	"eprints2-1"     => "2.1"
 );
 
-($type) = @ARGV;
+my( $type ) = @ARGV;
 
-$EPRINTS_VERSION = "2.0.1";
-if( defined $type && $ids{$type} )
-{
-	$EPRINTS_VERSION = $ids{$type};
+if( !defined $type || $type eq "" ) 
+{ 
+	print "NO TYPE!\n"; 
+	exit 1; 
 }
-$DATE = `date +%Y-%m-%d`;
-chomp $DATE;
 
-$whoami = `whoami`;
-chomp $whoami;
-$ENV{"CVSROOT"} = ":pserver:$whoami\@cvs.iam.ecs.soton.ac.uk:/home/iamcvs/CVS";
-# Get all the vars we need.
-$ntype = -1;
-if (!defined($type) || $type eq "nightly")
-{
+my $version_tag;
+my $package_version;
+my $package_desc;
+my $package_file;
+
+my $date = `date +%Y-%m-%d`;
+chomp $date;
+
+if( $type eq "nightly" ) 
+{ 
 	$version_tag = "HEAD";
-	$package_version = $ids{latest}."-".$DATE;
+	$package_version = "eprints-2-cvs-".$date;
 	$package_desc = "EPrints Nightly Build - $package_version";
-	$package_file = "eprints2-nightly-$DATE";
-	$ntype = 0;
+	$package_file = "eprints-2-cvs-$date";
 }
 else
 {
@@ -56,180 +59,133 @@ else
 		exit;
 	}
 	$version_tag = $type;
-	$package_version = $EPRINTS_VERSION;
-	$package_desc = "EPrints $EPRINTS_VERSION (".$codenames{$type}.") [Born on $DATE]";
+	$package_version = $ids{type};
+	$package_desc = "EPrints $ids{type} (".$codenames{$type}.") [Born on $date]";
 	$package_file = "eprints-".$ids{$type};
-	$ntype = 1;
 	print "YAY - $ids{$type}\n";
 }
 
-do_package($version_tag, $package_version, $package_desc, "licenses/gplin.txt", $package_file, $ntype);
+my $whoami = `whoami`;
+chomp $whoami;
+$ENV{"CVSROOT"} = ":pserver:$whoami\@cvs.iam.ecs.soton.ac.uk:/home/iamcvs/CVS";
 
-########################################
+my $license_file = "licenses/gplin.txt";
 
-sub insert_data
+erase_dir( "package" );
+erase_dir( "export" );
+
+print "Making directories...\n";
+mkdir("package") or die "Couldn't create package directory\n";
+mkdir("export") or die "Couldn't create export directory\n";
+
+print "Exporting from CVS...\n";
+my $originaldir = getcwd();
+chdir "export";
+system("cvs export -r $version_tag eprints/system >/dev/null")==0 or die "Could not export system.\n";
+system("cvs export -r $version_tag eprints/docs_ep2 >/dev/null")==0 or die "Could not export docs.\n";
+print "Removing .cvsignore files...\n";
+system("/bin/rm `find . -name '.cvsignore'`")==0 or die "Couldn't remove.";
+chdir "eprints/system";
+
+my @installerfiles = ( 
+	'aclocal.m4',
+	'autogen.sh',
+	'configure.in',
+	'df-check.pl',
+	'install.pl',
+	'install.pl.in' );
+foreach( @installerfiles )
 {
-	my( $key, $value, $source, $multiline) = @_;
+	system("cp $originaldir/$_ $_");
+}
+system("./autogen.sh");
 
-	open IN, $source or die "Unable to open source file.\n";
-	my $perms = (stat IN)[2];
-
-	open OUT, ">$source.out" or die "Unable to open output file.\n";
-
-	while ( <IN> )
+my @files = @installerfiles;
+foreach my $dir ( "archivecfg", "bin", "cgi", "cgi/users", "lib" )
+{
+	opendir( DIR, $dir );
+	my $file;
+	while( $file = readdir( DIR ) )
 	{
-		if( $multiline )
-		{
-			if( /$key/ )
-			{
-				my $line;
-				foreach $line ( split( "\n", $value ) )
-				{
-					my $l2 = $_;
-					$l2=~s/$key/$line/;
-					print OUT $l2;
-				}
-			}
-			else
-			{
-				print OUT $_;
-			}
-		}
-		else
-		{
-			s/$key/$value/g;
-			print OUT $_;
-		}
+		next if $file=~m/^\./; # not if it starts with .
+		next unless( -f "$dir/$file" );
+		push @files, "$dir/$file";
 	}
-
-	close OUT;
-	close IN;
-	system( "mv", "$source.out", "$source" );
-	chmod $perms, $source or die "Unable to chmod: $!";
-
-	return 0;
+	closedir( DIR );
+}
+my $license = "";
+open( FILE, "$originaldir/$license_file" );
+while( <FILE> ) { $license .= $_; }
+close FILE;
+my $genericpod = "";
+open( FILE, "pod/generic.pod" );
+while( <FILE> ) { $genericpod .= $_; }
+close FILE;
+foreach my $file ( @files )
+{
+	insert_data( "__GENERICPOD__", $genericpod, $file, 1 );
+	insert_data( "__LICENSE__", $license, $file, 1 );
+	insert_data( "__VERSION__", $package_version, $file );
 }
 
-sub do_package
+
+# Build docs - cjg Mike this needs to be smarter about what to copy (alpha/beta etc)
+print "Build Docs...\n"	;
+chdir $originaldir."/export/eprints/docs_ep2";
+`./mkdocs.pl`;
+	
+print "Making tarfile...\n";
+chdir $originaldir."/package";
+
+print "Making dirs...\n";
+open(DIRCONF, "$originaldir/conf/dirs.conf");
+mkdir("eprints");
+while(<DIRCONF>)
 {
-	my($version_tag, $package_version, $package_desc, $license_file, $package_file, $type_num) = @_;
-	if (-d "package")
-	{
-		system("/bin/rm -rf package")==0 or die "Couldn't remove package dir.\n";
-	}
-	
-	if (-d "export")
-	{
-		system("/bin/rm -rf export")==0 or die "Couldn't remove export dir.\n";
-	}
-	
-	print "Making directories...\n";
-	mkdir("package") or die "Couldn't create package directory\n";
-	mkdir("export") or die "Couldn't create export directory\n";
+	chomp;
+	mkdir("eprints/$_");
+}
+close(DIRCONF);
 
-	print "Exporting from CVS...\n";
-	$originaldir = getcwd();
-	chdir "export";
-	system("cvs export -r $version_tag eprints/system >/dev/null")==0 or die "Could not export system.\n";
-	system("cvs export -r $version_tag eprints/docs_ep2 >/dev/null")==0 or die "Could not export docs.\n";
-	print "Removing .cvsignore files...\n";
-	system("/bin/rm `find . -name '.cvsignore'`")==0 or die "Couldn't remove.";
-	print "Copying installer...\n";
-	system("cp $originaldir/install-eprints.pl eprints/system/install-eprints.pl");
-	print "Inserting license information...\n";
-	chdir "eprints/system";
+print "Copying files...\n";
+open(FILECONF, "$originaldir/conf/files.conf");
+while(<FILECONF>)
+{
+	chomp;
+	next if /^#/;
+	s/\s*#.*$//;
+	next if /^\s*$/;
+	my( $from, $to, $recurse ) = split /\t/;
+	my $recstr = "";
+	$recstr = "-r" if (defined $recurse);
+	my $cmd = "cp $recstr $originaldir/export/$from eprints/$to";
+	print "$cmd\n";
+	system( $cmd );
+}
+close(FILECONF);
 
-	@files = 'install-eprints.pl';
-	my $dir;
-	foreach $dir ( "archivecfg", "bin", "cgi", "cgi/users", "lib" )
-	{
-		opendir( DIR, $dir );
-		my $file;
-		while( $file = readdir( DIR ) )
-		{
-			next if $file=~m/^\./; # not if it starts with .
-			next unless( -f "$dir/$file" );
-			push @files, "$dir/$file";
-		}
-		closedir( DIR );
-	}
-	my $license = "";
-	open( FILE, "$originaldir/$license_file" );
-	while( <FILE> ) { $license .= $_; }
-	close FILE;
-	my $genericpod = "";
-	open( FILE, "pod/generic.pod" );
-	while( <FILE> ) { $genericpod .= $_; }
-	close FILE;
-	foreach $file (@files)
-	{
-		insert_data( "__GENERICPOD__", $genericpod, $file, 1 );
-		insert_data( "__LICENSE__", $license, $file, 1 );
-		insert_data( "__VERSION__", $package_version, $file );
-	}
+chdir $originaldir."/package";
 
+# Add documents dir
+mkdir("eprints/html/documents");
 
-	# Build docs - cjg Mike this needs to be smarter about what to copy (alpha/beta etc)
-	print "Build Docs...\n"	;
-	chdir $originaldir."/export/eprints/docs_ep2";
-	`./mkdocs.pl`;
-	
-	print "Making tarfile...\n";
-	chdir $originaldir."/package";
+# Do version
+open(FILEOUT, ">eprints/VERSION");
+print FILEOUT $package_version."\n";
+print FILEOUT $package_desc."\n";
+close(FILEOUT);
 
-	print "Making dirs...\n";
-	
-	open(DIRCONF, "$originaldir/conf/dirs.conf");
-	mkdir("eprints");
-	while(<DIRCONF>)
-	{
-		chomp;
-		mkdir("eprints/$_");
-	}
-	close(DIRCONF);
-
-	print "Copying files...\n";
-	open(FILECONF, "$originaldir/conf/files.conf");
-	while(<FILECONF>)
-	{
-		chomp;
-		next if /^#/;
-		s/\s*#.*$//;
-		next if /^\s*$/;
-		($from, $to, $recurse) = split /\t/;
-		$recstr = "";
-		$recstr = "-r" if (defined $recurse);
-		my $cmd = "cp $recstr $originaldir/export/$from eprints/$to";
-		print "$cmd\n";
-		system( $cmd );
-	}
-	close(FILECONF);
-
-	chdir $originaldir."/package";
-
-	# Add documents dir
-	mkdir("eprints/html/documents");
-
-	# Do version
-	open(FILEOUT, ">eprints/VERSION");
-	print FILEOUT $package_version."\n";
-	print FILEOUT $package_desc."\n";
-	close(FILEOUT);
-
-	# Do phrases
-	@langs = ("en" );
-	@files = ();
-
-	# Build up list from export.
-	opendir(PHRSDIR, "$originaldir/export/eprints/system/phrases");
-	while($item = readdir(PHRSDIR))
-	{
-		if (-d $item || $item =~ /^\./ ) { next; }	
-		push(@files, $item);
-	}
-	closedir(PHRSDIR);
-
-	# Nasty...
+# my @phrasefiles = ();
+# # Build up list from export.
+# opendir(PHRSDIR, "$originaldir/export/eprints/system/phrases");
+# while($item = readdir(PHRSDIR))
+# {
+# if (-d $item || $item =~ /^\./ ) { next; }	
+# push(@phrasefiles, $item);
+# }
+# closedir(PHRSDIR);
+# 
+# Nasty...
 #	foreach $l (@langs)
 #	{
 #		$currarch = 0;
@@ -279,7 +235,7 @@ sub do_package
 #	}
 #	# ...Nasty
 #
-#	if ($type_num == 0)
+#	if ($is_proper_release == 0)
 #	{
 #		# Here we copy over the nightly language files
 #		foreach $l (@langs)
@@ -296,7 +252,7 @@ sub do_package
 #			}
 #		}
 #	}
-#	elsif($type_num == 1)
+#	elsif($is_proper_release == 1)
 #	{
 #		# Here we copy over the Alpha language files
 #		foreach $l (@langs)
@@ -313,37 +269,103 @@ sub do_package
 #			}
 #		}
 #	}
-	foreach $l (@langs)
-	{
-		if (-e "$originaldir/export/eprints/system/phrases/system-$l-current")
-		{
-			system("cp $originaldir/export/eprints/system/phrases/system-$l-current eprints/cfg/system-phrases-$l.xml");
-		}
-		if (-e "$originaldir/export/eprints/system/phrases/archive-$l-current")
-		{
-			system("cp $originaldir/export/eprints/system/phrases/archive-$l-current eprints/defaultcfg/phrases-$l.xml");
-		}
-	}
 
-	# system("cp $originaldir/export/eprints/system/cgi/users/.htaccess eprints/cgi/users/.htaccess");
-	system("cp $originaldir/licenses/gpl.txt eprints/COPYING");
-	system("chmod -R g-w eprints")==0 or die("Couldn't change permissions on eprints dir.\n");
-	system("mv eprints $package_file")==0 or die("Couldn't move eprints dir to $package_file.\n");
-	system("tar czf ../$package_file.tar.gz $package_file")==0 or die("Couldn't tar up $package_file");
-	chdir $originaldir;
+# Do phrases
+my @langs = ("en" );
+
+foreach my $l ( @langs )
+{
+	if( -e "$originaldir/export/eprints/system/phrases/system-$l-current" )
+	{
+		system("cp $originaldir/export/eprints/system/phrases/system-$l-current eprints/cfg/system-phrases-$l.xml");
+	}
+	if( -e "$originaldir/export/eprints/system/phrases/archive-$l-current" )
+	{
+		system("cp $originaldir/export/eprints/system/phrases/archive-$l-current eprints/defaultcfg/phrases-$l.xml");
+	}
+}
+
+print "Inserting license information...\n";
+system("cp $originaldir/licenses/gpl.txt eprints/COPYING");
+system("rm eprints/perl_lib/EPrints/SystemSettings.pm");
+system("chmod -R g-w eprints")==0 or die("Couldn't change permissions on eprints dir.\n");
+
+system("mv eprints $package_file")==0 or die("Couldn't move eprints dir to $package_file.\n");
+
+exit;#!!!!!!!!
+
+my $tarfile = "../".$package_file.".tar.gz";
+if( -e $tarfile ) { system( "rm $tarfile" ); }
+system("tar czf $tarfile $package_file")==0 or die("Couldn't tar up $package_file");
+chdir $originaldir;
 
 	
 
-	print "Removing temporary directories...\n";
-	#system("/bin/rm -rf package")==0 or die("Couldn't remove package dir.\n");
-	#system("/bin/rm -rf export")==0 or die("Couldn't remove export dir.\n");
+print "Removing temporary directories...\n";
 
-	print "Done.\n";
+erase_dir( "package" );
+erase_dir( "export" );
 
+print "Done.\n";
+
+exit;
+
+
+
+
+#####################
+
+sub insert_data
+{
+	my( $key, $value, $source, $multiline) = @_;
+
+	open IN, $source or die "Unable to open source file.\n";
+	my $perms = (stat IN)[2];
+
+	open OUT, ">$source.out" or die "Unable to open output file.\n";
+
+	while ( <IN> )
+	{
+		if( $multiline )
+		{
+			if( /$key/ )
+			{
+				my $line;
+				foreach $line ( split( "\n", $value ) )
+				{
+					my $l2 = $_;
+					$l2=~s/$key/$line/;
+					print OUT $l2;
+				}
+			}
+			else
+			{
+				print OUT $_;
+			}
+		}
+		else
+		{
+			s/$key/$value/g;
+			print OUT $_;
+		}
+	}
+
+	close OUT;
+	close IN;
+	system( "mv", "$source.out", "$source" );
+	chmod $perms, $source or die "Unable to chmod: $!";
+
+	return 0;
 }
 
+sub erase_dir
+{
+	my( $dirname ) = @_;
 
-
-
-
-
+	if (-d $dirname )
+	{
+		system( "/bin/rm -rf ".$dirname ) == 0 or 
+			die "Couldn't remove ".$dirname." dir.\n";
+	}
+}
+	
