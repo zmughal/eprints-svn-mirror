e122afa65744edb755c799ad13294e07cca56d65
nobody <nobody@9491667e-5006-0410-a446-efbe8990b998>
2004-11-25 18:09:29 +0000

    This commit was manufactured by cvs2svn to create tag 'eprints2-3-7'.

---
+++ tags/eprints2-3-7/system/lib/MetaField/Pagerange.pm
@@ -0,0 +1,139 @@
+######################################################################
+#
+# EPrints::MetaField::Pagerange;
+#
+######################################################################
+#
+#  __COPYRIGHT__
+#
+# Copyright 2000-2008 University of Southampton. All Rights Reserved.
+# 
+#  __LICENSE__
+#
+######################################################################
+
+=pod
+
+=head1 NAME
+
+B<EPrints::MetaField::Pagerange> - no description
+
+=head1 DESCRIPTION
+
+not done
+
+=over 4
+
+=cut
+
+package EPrints::MetaField::Pagerange;
+
+use strict;
+use warnings;
+
+BEGIN
+{
+	our( @ISA );
+
+	@ISA = qw( EPrints::MetaField::Text );
+}
+
+use EPrints::MetaField::Text;
+
+# note that this renders pages ranges differently from
+# eprints 2.2
+sub render_single_value
+{
+	my( $self, $session, $value, $dont_link ) = @_;
+
+	unless( $value =~ m/^(\d+)-(\d+)$/ )
+	{
+		# value not in expected form. Ah, well. Muddle through.
+		return $session->make_text( $value );
+	}
+
+	my( $a, $b ) = ( $1, $2 );
+
+	# possibly there could be a non-breaking space after p.?
+
+	if( $a == $b )
+	{
+		my $frag = $session->make_doc_fragment();
+		$frag->appendChild( $session->make_text( "p." ) );
+		$frag->appendChild( $session->render_nbsp );
+		$frag->appendChild( $session->make_text( $a ) );
+	}
+
+#	consider compressing pageranges so that
+#	207-209 is rendered as 207-9
+#
+#       if( length $a == length $b )
+#       {
+#       }
+
+	my $frag = $session->make_doc_fragment();
+	$frag->appendChild( $session->make_text( "pp." ) );
+	$frag->appendChild( $session->render_nbsp );
+	$frag->appendChild( $session->make_text( $a.'-'.$b ) );
+
+	return $frag;
+}
+
+sub get_basic_input_elements
+{
+	my( $self, $session, $value, $suffix, $staff, $obj ) = @_;
+
+	my @pages = split /-/, $value if( defined $value );
+ 	my $fromid = $self->{name}.$suffix."_from";
+ 	my $toid = $self->{name}.$suffix."_to";
+		
+	my $frag = $session->make_doc_fragment;
+
+	$frag->appendChild( $session->make_element(
+		"input",
+		"accept-charset" => "utf-8",
+		name => $fromid,
+		value => $pages[0],
+		size => 6,
+		maxlength => 120 ) );
+
+	$frag->appendChild( $session->make_text(" ") );
+	$frag->appendChild( $session->html_phrase( 
+		"lib/metafield:to" ) );
+	$frag->appendChild( $session->make_text(" ") );
+
+	$frag->appendChild( $session->make_element(
+		"input",
+		"accept-charset" => "utf-8",
+		name => $toid,
+		value => $pages[1],
+		size => 6,
+		maxlength => 120 ) );
+
+	return [ [ { el=>$frag } ] ];
+}
+
+sub is_browsable
+{
+	return( 1 );
+}
+
+sub form_value_basic
+{
+	my( $self, $session, $suffix ) = @_;
+	
+	my $from = $session->param( $self->{name}.$suffix."_from" );
+	my $to = $session->param( $self->{name}.$suffix."_to" );
+
+	if( !defined $to || $to eq "" )
+	{
+		return( $from );
+	}
+		
+	return( $from . "-" . $to );
+}
+
+sub get_search_group { return 'pagerange'; } 
+
+######################################################################
+1;
---
+++ tags/eprints2-3-7/system/lib/Rewrite.pm
@@ -0,0 +1,133 @@
+
+
+package EPrints::Rewrite;
+
+use EPrints::AnApache;  
+use EPrints::Session;
+
+use strict;
+  
+sub handler 
+{
+	my( $r ) = @_;
+
+	my $archiveid = $r->dir_config( "EPrints_ArchiveID" );
+	if( !defined $archiveid )
+	{
+		return DECLINED;
+	}
+	my $archive = EPrints::Archive->new_archive_by_id( $archiveid );
+	my $urlpath = $archive->get_conf( "urlpath" );
+	my $uri = $r->uri;
+	my $lang = EPrints::Session::get_session_language( $archive, $r );
+	my $args = $r->args;
+	if( $args ne "" ) { $args = '?'.$args; }
+
+	# REMOVE the urlpath if any!
+	unless( $uri =~ s#^$urlpath## )
+	{
+		return DECLINED;
+	}
+
+	if( $uri =~ m#^/perl/# )
+	{
+		return DECLINED;
+	} 
+
+	if( $uri =~ m#^/cgi/# )
+	{
+		# In case people want a real CGI Directory on the same
+		# server. For example for mimetex.
+		return DECLINED;
+	} 
+
+	
+	# shorturl does not (yet) effect secure docs.
+	if( $uri =~ s#^/secure/([0-9]+)([0-9][0-9])([0-9][0-9])([0-9][0-9])#/secure/$1/$2/$3/$4# )
+	{
+		$r->filename( $archive->get_conf( "htdocs_path" )."/".$uri );
+		return OK;
+	}
+
+
+	my $shorturl = $archive->get_conf( "use_short_urls" );
+	$shorturl = 0 unless( defined $shorturl );
+
+	#$uri =~ s#^/archive/([0-9]+)([0-9][0-9])([0-9][0-9])([0-9][0-9])#/archive/$1/$2/$3/$4#;
+	if( $uri =~ m#^/archive/([0-9]+)(.*)$# )
+	{
+		# is a long record url
+		my $n = $1;
+		my $tail = $2;
+		my $redir =0;
+		if( $tail eq "" ) { $tail = "/"; $redir=1 }
+		
+		if( $shorturl )
+		{
+			# redirect to short form
+			return redir( $r, sprintf( "%s/%d%s",$urlpath, $n, $tail ).$args );
+		}
+
+		my $s8 = sprintf('%08d',$n);
+		$s8 =~ m/(..)(..)(..)(..)/;	
+		if( length $n < 8 || $redir)
+		{
+			# not enough zeros, redirect to correct version
+			return redir( $r, sprintf( "%s/archive/%08d%s",$urlpath, $n, $tail ).$args );
+		}
+		$uri = "/archive/$1/$2/$3/$4$tail";
+	}
+
+	
+	if( $uri =~ m#^/([0-9]+)(.*)$# )
+	{
+		# is a shorturl record url
+		my $n = $1;
+		my $tail = $2;
+		my $redir = 0;
+		if( $tail eq "" ) { $tail = "/"; $redir = 1; }
+		if( !$shorturl )
+		{
+			# redir to long form
+			return redir( $r, sprintf( "%s/archive/%08d%s",$urlpath, $n, $tail ).$args);
+		} 
+
+		if( ($n + 0) ne $n || $redir)
+		{
+			# leading zeros
+			return redir( $r, sprintf( "%s/%d%s",$urlpath, $n, $tail ).$args );
+		}
+		my $s8 = sprintf('%08d',$n);
+		$s8 =~ m/(..)(..)(..)(..)/;	
+		$uri = "/archive/$1/$2/$3/$4$tail";
+	}
+
+	# apache 2 does not automatically look for index.html so we have to do it ourselves
+	if( $uri =~ m#/$# )
+	{
+		$r->filename( $archive->get_conf( "htdocs_path" )."/".$lang.$uri."index.html" );
+	}
+	else
+	{
+		$r->filename( $archive->get_conf( "htdocs_path" )."/".$lang.$uri );
+	}
+
+	return OK;
+}
+
+
+sub redir
+{
+	my( $r, $url ) = @_;
+
+	$r->status_line( "302 Close but no Cigar" );
+	EPrints::AnApache::header_out( $r, "Location", $url );
+	$r->send_http_header;
+	return DONE;
+} 
+
+
+
+1;
+
+
