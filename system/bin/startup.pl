815cb610acf61bc2a4dc0521d47e0dba489cf44e
nobody <nobody@9491667e-5006-0410-a446-efbe8990b998>
2005-03-21 17:12:07 +0000

    This commit was manufactured by cvs2svn to create tag 'eprints2-3-11-99-2'.

--- system/bin/startup.pl
+++ system/bin/startup.pl
@@ -1,4 +1,4 @@
-use lib '/opt/eprints2/perl_lib';
+use lib '/opt/ep2stable/perl_lib';
 
 ######################################################################
 #
@@ -10,38 +10,39 @@ use lib '/opt/eprints2/perl_lib';
 #
 ######################################################################
 
-print STDERR "EPRINTS: Loading Core Modules\n";
-
 ## Apache::DBI MUST come before other modules using DBI or
 ## you won't get constant connections and everything
 ## will go horribly wrong...
 
 use Carp qw(verbose);
 
+use EPrints::AnApache;
+
 use Apache::DBI;
 #$Apache::DBI::DEBUG = 3;
-use Apache::Registry;          
 
+$ENV{MOD_PERL} or EPrints::Utils::abort( "not running under mod_perl!" );
 
 use EPrints::XML;
 use EPrints::Utils;
 use EPrints::Config;
 
-$ENV{MOD_PERL} or EPrints::Utils::abort( "not running under mod_perl!" );
-
 # This code is interpreted *once* when the server starts
+use EPrints::Archive;
 use EPrints::Auth;
 use EPrints::Database;
 use EPrints::Document;
 use EPrints::EPrint;
+use EPrints::Extras;
 use EPrints::ImportXML;
 use EPrints::Language;
 use EPrints::Latex;
 use EPrints::MetaField;
 use EPrints::OpenArchives;
-use EPrints::Archive;
+use EPrints::Rewrite;
 use EPrints::SearchExpression;
 use EPrints::SearchField;
+use EPrints::SearchCondition;
 use EPrints::Session;
 use EPrints::Subject;
 use EPrints::SubmissionForm;
@@ -54,20 +55,19 @@ use EPrints::Paracite;
 
 use strict;
 
-print STDERR "EPRINTS: Core Modules Loaded\n";
 
-# cjg SYSTEM CONF SHOULD SAY IF TO PRELOAD OR NOT...
+EPrints::Config::ensure_init();
 
-print STDERR "EPRINTS: Loading Config Modules\n";
 my %done = ();
 foreach( EPrints::Config::get_archive_ids() )
 {
 	next if $done{$_};
 	EPrints::Archive->new_archive_by_id( $_ );
 }
-print STDERR "EPRINTS: Config Modules Loaded\n";
+print STDERR "EPrints archives loaded: ".join( ", ",  EPrints::Config::get_archive_ids() )."\n";
 
 # Tell me more about warnings
 $SIG{__WARN__} = \&Carp::cluck;
 
+$EPrints::SystemSettings::loaded = 1;
 1;
