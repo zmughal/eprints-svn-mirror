8155913a4c6c5014bcdc0561452660b38c2d9c75
cjg <cjg@9491667e-5006-0410-a446-efbe8990b998>
2005-11-21 10:58:22 +0000

    - Merged Plugins.pm into being static methods of Plugin.pm

--- branches/eprints2/system/lib/Plugin.pm
+++ branches/eprints2/system/lib/Plugin.pm
@@ -83,3 +83,117 @@ sub defaults
 =cut
 ######################################################################
 
+
+
+######################################################################
+# STATIC METHODS
+######################################################################
+
+
+use EPrints::Archive;
+use EPrints::SystemSettings;
+$EPrints::Plugin::REGISTRY = {};
+
+EPrints::Plugin::load();
+
+######################################################################
+=pod
+
+=item EPrints::Plugin::load() [static]
+
+Load all system plugins 
+
+=cut
+######################################################################
+
+sub load
+{
+	# no opts
+
+	my $dir = $EPrints::SystemSettings::conf->{base_path}."/perl_lib/EPrints/Plugin";
+
+	load_dir( $EPrints::Plugin::REGISTRY, $dir, "EPrints::Plugin" );
+}
+
+
+######################################################################
+=pod
+
+=item EPrints::Plugin::load_dir( $reg, $path, $baseclass, @prefix ) [static]
+
+Load plugins in this directory and recurse through subdirectories.
+
+$reg is a pointer to a hash to store the lost of found plugins in.
+
+=cut
+######################################################################
+
+sub load_dir
+{
+	my( $reg, $path, $baseclass, @prefix ) = @_;
+
+	my $dh;
+	opendir( $dh, $path ) || die "Could not open $path";
+	while( my $fn = readdir( $dh ) )
+	{
+		next if( $fn =~ m/^\./ );
+		next if( $fn eq "CVS" );
+		my $filename = "$path/$fn";
+		if( -d $filename )
+		{
+			load_dir( $reg, $filename, $baseclass, @prefix, $fn );
+			next;
+		}
+
+		next unless( $fn =~ s/\.pm// );
+		my $class = $baseclass."::".join("::",@prefix,$fn );
+		
+		eval "use $class";
+
+		no strict "refs";
+		my $absvar = $class.'::ABSTRACT';
+		my $abstract = ${$absvar};
+		use strict "refs";
+		next if( $abstract );
+		my $pluginid = $class->id;
+
+		$reg->{$pluginid} = $class;
+	}
+	closedir( $dh );
+
+}
+
+
+
+
+######################################################################
+=pod
+
+=item @plugin_ids  = EPrints::Plugin::plugin_list() [static]
+
+Return either a list of all the ids of the system plugins.
+
+=cut
+######################################################################
+
+sub plugin_list
+{
+	my( $self ) = @_;
+
+	return keys %{$EPrints::Plugin::REGISTRY};
+}
+
+
+
+
+
+1;
+
+######################################################################
+=pod
+
+=back
+
+=cut
+######################################################################
+
