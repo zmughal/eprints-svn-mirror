f095d01437267890157498b1746afae3f796437b
cjg <cjg@9491667e-5006-0410-a446-efbe8990b998>
2005-11-25 14:25:49 +0000

    - Cleaned up DIDL in accordance with Herbert van de Sompel's     suggestions. (different namespaces identifier not DC in     components).

--- branches/eprints2/system/archivecfg/ArchiveOAIConfig.pm
+++ branches/eprints2/system/archivecfg/ArchiveOAIConfig.pm
@@ -43,7 +43,7 @@ $oai->{v2}->{metadata_namespaces} =
 $oai->{v2}->{metadata_schemas} =
 {
 	"oai_dc"    =>  "http://www.openarchives.org/OAI/2.0/oai_dc.xsd",
-	"didl"      =>  "http://purl.lanl.gov/STB-RL/schemas/2004-11/DIDL.xsd",
+	"didl"      =>  "http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-21_schema_files/did/didmodel.xsd",
 };
 
 # Each supported metadata format will need a function to turn
@@ -345,7 +345,7 @@ sub eprint_to_unqualified_dc
 	my $date = $eprint->get_value( "date_effective" );
 	if( defined $date )
 	{
-        	$date =~ s/(-0+)+$//;
+		$date =~ s/(-0+)+$//;
 		push @dcdata, [ "date", $date ];
 	}
 
@@ -393,22 +393,24 @@ sub eprint_to_didl
 {
 	my( $eprint, $session ) = @_;
 
-	my $didl = $session->make_element( "didl:DIDL",
-                  "xmlns:didl"=>"urn:mpeg:mpeg21:2002:02-DIDL-NS",
-                  "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance",
-                  "xsi:schemaLocation"=>"urn:mpeg:mpeg21:2002:02-DIDL-NS 
-                         http://purl.lanl.gov/STB-RL/schemas/2004-11/DIDL.xsd" );
+	my $didl = $session->make_element( 
+		"didl:DIDL",
+		"xmlns:didl"=>"urn:mpeg:mpeg21:2002:02-DIDL-NS",
+		"xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance",
+		"xsi:schemaLocation"=>"urn:mpeg:mpeg21:2002:02-DIDL-NS 
+			 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-21_schema_files/did/didmodel.xsd" );
 	my $item = $session->make_element( "didl:Item" );
 	$didl->appendChild( $item );
 
 
 	my $d1 = $session->make_element( "didl:Descriptior" );
-	my $s1 = $session->make_element( "didl:Statement", mimeType=>"text/xml; charset=UTF-8" );
-	my $ident = $session->make_element( "dii:Identifier",
-                              "xmlns:dii"=>"urn:mpeg:mpeg21:2002:01-DII-NS",
-                              "xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance",
-                              "xsi:schemaLocation"=>"urn:mpeg:mpeg21:2002:01-DII-NS
-                              http://purl.lanl.gov/STB-RL/schemas/2003-09/DII.xsd" );
+	my $s1 = $session->make_element( "didl:Statement", mimeType=>"application/xml; charset=utf-8" );
+	my $ident = $session->make_element( 
+		"dii:Identifier",
+		"xmlns:dii"=>"urn:mpeg:mpeg21:2002:01-DII-NS",
+		"xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance",
+		"xsi:schemaLocation"=>"urn:mpeg:mpeg21:2002:01-DII-NS
+		 	http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-21_schema_files/dii/dii.xsd" );
 	$ident->appendChild( $session->make_text( $eprint->get_url ) );
 	$s1->appendChild( $ident );
 	$d1->appendChild( $s1 );
@@ -416,7 +418,7 @@ sub eprint_to_didl
 
 
 	my $d2 = $session->make_element( "didl:Descriptior" );
-	my $s2 = $session->make_element( "didl:Statement", mimeType=>"text/xml; charset=UTF-8" );
+	my $s2 = $session->make_element( "didl:Statement", mimeType=>"application/xml; charset=utf-8" );
 	$s2->appendChild( make_metadata_oai_dc_oai2( $eprint, $session ) );
 	$d2->appendChild( $s2 );
 	$item->appendChild( $d2 );
@@ -426,23 +428,21 @@ sub eprint_to_didl
 	{
 		my $comp = $session->make_element( "didl:Component" );
 		$item->appendChild( $comp );
-		$cdesc = $session->make_element( "didl:Descriptior" );
-		$comp->appendChild( $cdesc );
-		my $dc = $session->make_element(
-			"dc",
-			"xmlns" => "http://www.openarchives.org/OAI/2.0/oai_dc/",
-			"xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance",
-			"xsi:schemaLocation" =>
-	 	"http://www.openarchives.org/OAI/2.0/oai_dc/ http://www.openarchives.org/OAI/2.0/oai_dc.xsd" );
-		my $format = $mimetypes->{$doc->get_value("format")};
-		$format = "application/octet-stream" unless defined $format;
-		my $dcformat1 = $session->make_element( "dc:format" );
-		$dcformat1->appendChild( $session->make_text( $format ));
-		my $dcident = $session->make_element( "dc:identifier" );
-		$dcident->appendChild( $session->make_text( $doc->get_baseurl ));
-		$dc->appendChild( $dcformat1 );	
-		$dc->appendChild( $dcident );	
-		$cdesc->appendChild( $dc );
+
+
+		my $d3 = $session->make_element( "didl:Descriptior" );
+		my $s3 = $session->make_element( "didl:Statement", mimeType=>"application/xml; charset=utf-8" );
+		my $i3 = $session->make_element( 
+			"dii:Identifier",
+			"xmlns:dii"=>"urn:mpeg:mpeg21:2002:01-DII-NS",
+			"xmlns:xsi"=>"http://www.w3.org/2001/XMLSchema-instance",
+			"xsi:schemaLocation"=>"urn:mpeg:mpeg21:2002:01-DII-NS
+		 	    http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-21_schema_files/dii/dii.xsd" );
+		$i3->appendChild( $session->make_text( $doc->get_baseurl ) );
+		$s3->appendChild( $i3 );
+		$d3->appendChild( $s3 );
+		$comp->appendChild( $d3 );
+
 		my %files = $doc->files;
 		foreach my $file ( keys %files )
 		{
@@ -453,60 +453,76 @@ sub eprint_to_didl
 		}
 	}
 
-        
+	
 #    <didl:Resource mimeType="application/pdf" ref="http://amsacta.cib.unibo.it/archive/
- #              00000014/01/GaAs_1_Vorobiev.pdf"/>
+ #	      00000014/01/GaAs_1_Vorobiev.pdf"/>
 	return $didl;
 }
 
 
 my $x =<<END;
+corrected.
+	 <didl:DIDL
+		 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		 xmlns:didl="urn:mpeg:mpeg21:2002:02-DIDL-NS"
+		 xsi:schemaLocation="urn:mpeg:mpeg21:2002:02-DIDL-NS
+		 	http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-21_schema_files/did/didmodel.xsd">
+	 <dii:Identifier
+		 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		 xsi:schemaLocation="urn:mpeg:mpeg21:2002:01-DII-NS
+		 http://standards.iso.org/ittf/PubliclyAvailableStandards/MPEG-21_schema_files/dii/dii.xsd"
+		 	xmlns:dii="urn:mpeg:mpeg21:2002:01-DII-NS">http://ep2stable.ecs.soton.ac.uk/45/</dii:Identifier>
+
+
+
+
+
 <didl:DIDL  xmlns:didl="urn:mpeg:mpeg21:2002:02-DIDL-NS"
-                  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                  xsi:schemaLocation="urn:mpeg:mpeg21:2002:02-DIDL-NS
-                  http://purl.lanl.gov/STB-RL/schemas/2004-11/DIDL.xsd">
+		  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		  xsi:schemaLocation="urn:mpeg:mpeg21:2002:02-DIDL-NS
+		  http://purl.lanl.gov/STB-RL/schemas/2004-11/DIDL.xsd">
 <didl:Item>
       <didl:Descriptor>
-            <didl:Statement mimeType="text/xml; charset=UTF-8">
-                  <dii:Identifier
-                              xmlns:dii="urn:mpeg:mpeg21:2002:01-DII-NS"
-                              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                              xsi:schemaLocation="urn:mpeg:mpeg21:2002:01-DII-NS
-                              http://purl.lanl.gov/STB-RL/schemas/2003-09/DII.xsd">
-                      http://amsacta.cib.unibo.it/archive/00000014/
-                  </dii:Identifier>
-            </didl:Statement>
+	    <didl:Statement mimeType="application/xml; charset=utf-8">
+		  <dii:Identifier
+			      xmlns:dii="urn:mpeg:mpeg21:2002:01-DII-NS"
+			      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+			      xsi:schemaLocation="urn:mpeg:mpeg21:2002:01-DII-NS
+			      http://purl.lanl.gov/STB-RL/schemas/2003-09/DII.xsd">
+		      http://amsacta.cib.unibo.it/archive/00000014/
+		  </dii:Identifier>
+	    </didl:Statement>
       </didl:Descriptor>
       <didl:Descriptor>
-            <didl:Statement mimeType="text/xml; charset=UTF-8">
-                 <oai_dc:dc xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                       xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/
-                       http://www.openarchives.org/OAI/2.0/oai_dc.xsd"
-                       xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
-                       xmlns:dc="http://purl.org/dc/elements/1.1/">
-                  <dc:title>A Simple Parallel-Plate Resonator Technique for Microwave.
-                      Characterization of Thin Resistive Films
-                  </dc:title>
-                  <dc:creator>Vorobiev, A.</dc:creator>
-                  <dc:subject>ING-INF/01 Elettronica</dc:subject>
-                  <dc:description>A parallel-plate resonator method is proposed for
-                      non-destructive characterisation of resistive films used in microwave
-                      integrated circuits. A slot made in one ...
-                  </dc:description>
-                  <dc:publisher>Microwave engineering Europe</dc:publisher>
-                  <dc:date>2002</dc:date>
-                  <dc:type>Documento relativo ad una Conferenza o altro Evento</dc:type>
-                  <dc:type>PeerReviewed</dc:type>
-                  <dc:identifier>
-                      http://amsacta.cib.unibo.it/archive/00000014/
-                  </dc:identifier>
-                  <dc:format>application/pdf</dc:format>
-                 </oai_dc:dc>
-            </didl:Statement>
+	    <didl:Statement mimeType="application/xml; charset=utf-8">
+		 <oai_dc:dc xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		       xsi:schemaLocation="http://www.openarchives.org/OAI/2.0/oai_dc/
+		       http://www.openarchives.org/OAI/2.0/oai_dc.xsd"
+		       xmlns:oai_dc="http://www.openarchives.org/OAI/2.0/oai_dc/"
+		       xmlns:dc="http://purl.org/dc/elements/1.1/">
+		  <dc:title>A Simple Parallel-Plate Resonator Technique for Microwave.
+		      Characterization of Thin Resistive Films
+		  </dc:title>
+		  <dc:creator>Vorobiev, A.</dc:creator>
+		  <dc:subject>ING-INF/01 Elettronica</dc:subject>
+		  <dc:description>A parallel-plate resonator method is proposed for
+		      non-destructive characterisation of resistive films used in microwave
+		      integrated circuits. A slot made in one ...
+		  </dc:description>
+		  <dc:publisher>Microwave engineering Europe</dc:publisher>
+		  <dc:date>2002</dc:date>
+		  <dc:type>Documento relativo ad una Conferenza o altro Evento</dc:type>
+		  <dc:type>PeerReviewed</dc:type>
+		  <dc:identifier>
+		      http://amsacta.cib.unibo.it/archive/00000014/
+		  </dc:identifier>
+		  <dc:format>application/pdf</dc:format>
+		 </oai_dc:dc>
+	    </didl:Statement>
       </didl:Descriptor>
       <didl:Component>
-            <didl:Resource mimeType="application/pdf" ref="http://amsacta.cib.unibo.it/archive/
-               00000014/01/GaAs_1_Vorobiev.pdf"/>
+	    <didl:Resource mimeType="application/pdf" ref="http://amsacta.cib.unibo.it/archive/
+	       00000014/01/GaAs_1_Vorobiev.pdf"/>
       </didl:Component>
 </didl:Item>
 </didl:DIDL>
