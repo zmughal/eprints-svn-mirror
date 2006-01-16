3d500d81deb2a87e16833e86483a483e202f7486
tmb <tmb@9491667e-5006-0410-a446-efbe8990b998>
2006-01-16 10:15:04 +0000

    - Created script to enter measures of esteem  - Created script to select items for RAE return  - Created script to generate/export RAE reports  - Created configuration for eprints.soton  - Created module to load config into current session  - Created test cases

---
+++ branches/eprints2/extensions/rae/sotoncfg/ArchiveRAEConfig.pm
@@ -0,0 +1,136 @@
+
+# Configuration for eprints@soton
+
+sub get_rae_conf {
+
+my $c = ();
+
+$c->{group_perms} = {
+	"500" => "uos-fp", #tmb
+	"341" => "uos-jf", # anw
+	"7985" => "uos-fp", # sf11
+};
+
+# 'Measures of esteem' fields
+$c->{rae_fields} = ["rae_conf", "rae_prof", "rae_gbod", "rae_extexam", "rae_vispos", "rae_advbod", "rae_ed", 
+	"rae_fell", "rae_lect", "rae_awards", "rae_hdeg", "rae_grants", "rae_papawards", "rae_rmono", 
+	"rae_patents", "rae_sorgs", "rae_rcent", "rae_consind", "rae_entract", "rae_sciboards", "rae_other"];
+
+# Id of the search used to select RAE items
+$c->{search_id} = "advanced";
+
+# Default values for the $searchexp
+# in context of current $user
+$c->{init} = sub {
+	my ($searchexp, $user) = @_;
+	
+	#print STDERR "Hello: ".$searchexp->render_description()->toString();
+			
+	#$searchexp->get_searchfield( "creators/editors" )->{value} = $user->get_value("name")->{family};
+	$searchexp->get_searchfield( "creators" )->{value} = $user->get_value("name")->{family};
+	
+	$searchexp->get_searchfield( "date_effective" )->{value} = "2003-";
+};
+
+# Grouping field for RAE reports
+$c->{group_by} = "dept";
+
+# Groups to exclude from the list
+$c->{exclude_group} = {
+	uos => 1,
+};
+
+# Show the group of 'all staff'
+$c->{show_all} = 1;
+
+# Check a selected item for problems in RAE report
+# $user is the user who has selected the $item
+# $others is the list of all users (inc. $user) who
+# have selected the item
+$c->{check_item} = sub { 
+	my ( $session, $user, $item, $others ) = @_;
+	my @problems;
+	# Check no other users have selected this item
+	my @names;
+	foreach my $otherid ( @$others )
+        {
+		next if $otherid == $user->get_id;
+                my $other = EPrints::User->new( $session, $otherid );
+                if( defined $other )
+                {
+                        push @names, EPrints::Utils::tree_to_utf8( $other->render_description );
+                } else {
+			push @names, $session->phrase( "rae:unknown_user", id => $otherid );
+		}
+        }
+        if( scalar( @names ) > 0 )
+        {
+		push @problems, $session->html_phrase( "rae/report:problem_multiple_users",
+                        names => $session->make_text( join(", ", @names) ) );
+        }	
+	# Check item has a document
+	my @docs = $item->get_all_documents();
+	if( scalar( @docs ) == 0 ) {
+		push @problems, $session->html_phrase( "rae/report:problem_no_doc",
+			type => $session->make_text( $item->get_value( "type" ) ) );
+	}
+	return \@problems;
+};
+
+# Print CSV header
+$c->{csv_header} = sub {
+	print csv_line('Dept', 'Username', 'Surname', 'First Name', 'Score', 'Publication', 'Paper');
+};
+
+
+# Print CSV row for a selected item
+# (called for each selected item)
+$c->{csv_row} = sub {
+	my ( $session, $user, $item ) = @_;
+	my $name = $user->get_value( 'name' );
+	my $book = $item->get_value( 'publication' );
+	$book = $item->get_value( 'event_title' ) if !defined $book;
+	print csv_line(
+		$user->get_value( "dept" ),
+		$user->get_value( "username" ),
+		$name->{family},
+		$name->{given},
+		'',
+		$book,
+		EPrints::Utils::tree_to_utf8( $item->render_citation ),
+	);
+};
+
+# Print CSV footer
+# $rows is the number of item rows already output
+$c->{csv_footer} = sub {
+	my ( $rows ) = @_;
+	print csv_line( '','','','','','','' );
+	my $range = 'E2:E'.($rows+1); # E1 is header row
+	print csv_line( 'Score','Label','Total Papers','','','','' );
+	print csv_line( '0', "Unclassified", '='.$rows."-INDEX(FREQUENCY($range,A".($rows+4).":A".($rows+8)."),2)-INDEX(FREQUENCY($range,A".($rows+5).":A".($rows+8)."),2)-INDEX(FREQUENCY($range,A".($rows+6).":A".($rows+8)."),2)-INDEX(FREQUENCY($range,A".($rows+7).":A".($rows+8)."),2)", '','','','' );
+	print csv_line( '1', "1*", "=INDEX(FREQUENCY($range,A".($rows+4).":A".($rows+8)."),2)",'','','','' );
+	print csv_line( '2', "2*", "=INDEX(FREQUENCY($range,A".($rows+5).":A".($rows+8)."),2)",'','','','' );
+	print csv_line( '3', "3*", "=INDEX(FREQUENCY($range,A".($rows+6).":A".($rows+8)."),2)",'','','','' );
+	print csv_line( '4', "4*", "=INDEX(FREQUENCY($range,A".($rows+7).":A".($rows+8)."),2)",'','','','' );
+};
+
+$c->{testconf} = "Hello World!";
+
+return $c;
+}
+
+
+sub csv_line
+{
+	# Format a list of values as a CSV row
+	my( @values ) = @_;
+	foreach( @values )
+	{
+		s/([\\\"])/\\$1/g;
+	}
+	return '"'.join( '","', @values ).'"'."\n";
+}
+
+
+1;
