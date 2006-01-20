33ae50b20df8b3c10737dc041fe0c7d832390083
tdb01r <tdb01r@9491667e-5006-0410-a446-efbe8990b998>
2006-01-20 14:57:22 +0000

    - Added PerlLogHandler to virtual host - Added example (test) LogHandler 	Stores requests in archives/<archive>/logs/handler.log

---
+++ branches/eprints2/system/perl_lib/EPrints/LogHandler.pm
@@ -0,0 +1,56 @@
+######################################################################
+#
+# EPrints::LogHandler
+#
+######################################################################
+#
+#  __COPYRIGHT__
+#
+# Copyright 2000-2008 University of Southampton. All Rights Reserved.
+# 
+#  __LICENSE__
+#
+######################################################################
+
+=pod
+
+=head1 NAME
+
+EPrints::LogHandler - Main handler for Apache log events
+
+=cut
+
+package EPrints::LogHandler;
+
+use strict;
+use warnings;
+
+use Fcntl qw(:flock);
+
+use EPrints::Session;
+use EPrints::AnApache;
+
+sub handler
+{
+	my( $r ) = shift;
+
+	my $session = new EPrints::Session;
+	return EPrints::AnApache::DECLINED unless defined $session;
+
+	# The rest of this is a hack to test with
+	my $archive = $session->get_archive();
+
+	my $root = $archive->get_conf( "archiveroot" );
+	my $logpath = "$root/logs";
+
+	mkdir($logpath);
+
+	my $logfile = "$logpath/handler.log";
+	open my $fh, ">>$logfile" or die "can't open $logfile: $!";
+	flock $fh, LOCK_EX;
+	print $fh sprintf("%d\t%s\t%s\n", time(), $archive->get_id, $r->uri);
+	close $fh;
+	return EPrints::AnApache::OK;
+}
+
+1;
