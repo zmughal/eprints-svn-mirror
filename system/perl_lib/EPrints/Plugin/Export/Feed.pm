c6436875b127a17e5e93c3cba395e3538e89bc7c
tdb01r <tdb01r@9491667e-5006-0410-a446-efbe8990b998>
2008-08-07 09:45:37 +0000

    * Merged [3775]  * Added "page" to Session::get_url/URL::get_url  * Added Plugin::icon_url and Screen::action_icon_url methods  * Use ->icon_url and ->action_icon_url to get icon URLs, fixing #3354  * REMOVED Export::icon()  * This changes behaviour in plugins for icons:   - s#/style/images/(.*)#$1/   - Makes Screen plugin icons consistent with Export plugin icons   - Any Plugin subclass can have an icon

--- system/perl_lib/EPrints/Plugin/Export/Feed.pm
+++ system/perl_lib/EPrints/Plugin/Export/Feed.pm
@@ -15,13 +15,12 @@ sub new
 	my $self = $class->SUPER::new( %opts );
 
 	$self->{name} = "Abstract Feed";
+	$self->{icon} = "feed-icon-14x14.png";
 	$self->{visible} = "";
 	
 	return $self;
 }
 
 sub is_feed { return 1; }
-
-sub icon { return "feed-icon-14x14.png"; }
 
 1;
