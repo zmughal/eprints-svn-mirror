01fdc57bbd1152515f872399d5d07ffb15e22182
cjg <cjg@9491667e-5006-0410-a446-efbe8990b998>
2008-10-29 13:24:32 +0000

    merged r3889 r3890 r3891

--- branches/3.1/system/perl_lib/EPrints/Update/Views.pm
+++ branches/3.1/system/perl_lib/EPrints/Update/Views.pm
@@ -127,7 +127,6 @@ sub update_view_file
 	}
 
 	my @view_path = split( '/', $viewinfo );
-	
 	update_view_list( $session, $view, $langid, \@view_path );
 }
 
@@ -400,7 +399,7 @@ sub update_view_menu
 			},
 			"browseindex" );
 
-	open( INCLUDE, ">$target.include" ) || EPrints::abort( "Failed to write $target.include: $!" );
+	open( INCLUDE, ">:utf8", "$target.include" ) || EPrints::abort( "Failed to write $target.include: $!" );
 	print INCLUDE EPrints::XML::to_string( $page );
 	close INCLUDE;
 
@@ -801,24 +800,24 @@ sub update_view_list
 
 
 		# This writes the title including HTML tags
-		open( TITLE, ">$page_file_name.title" ) || EPrints::abort( "Failed to write $page_file_name.title: $!" );
+		open( TITLE, ">:utf8", "$page_file_name.title" ) || EPrints::abort( "Failed to write $page_file_name.title: $!" );
 		print TITLE EPrints::XML::to_string( $title );
 		close TITLE;
 
 		# This writes the title with HTML tags stripped out.
-		open( TITLETXT, ">$page_file_name.title.textonly" ) || EPrints::abort( "Failed to write $page_file_name.title.textonly: $!" );
+		open( TITLETXT, ">:utf8", "$page_file_name.title.textonly" ) || EPrints::abort( "Failed to write $page_file_name.title.textonly: $!" );
 		print TITLETXT EPrints::Utils::tree_to_utf8( $title );
 		close TITLETXT;
 
 		if( defined $view->{template} )
 		{
-			open( TEMPLATE, ">$page_file_name.template" ) || EPrints::abort( "Failed to write $page_file_name.template: $!" );
+			open( TEMPLATE, ">:utf8", "$page_file_name.template" ) || EPrints::abort( "Failed to write $page_file_name.template: $!" );
 			print TEMPLATE $view->{template};
 			close TEMPLATE;
 		}
 
-		open( PAGE, ">$page_file_name.page" ) || EPrints::abort( "Failed to write $page_file_name.page: $!" );
-		open( INCLUDE, ">$page_file_name.include" ) || EPrints::abort( "Failed to write $page_file_name.include: $!" );
+		open( PAGE, ">:utf8", "$page_file_name.page" ) || EPrints::abort( "Failed to write $page_file_name.page: $!" );
+		open( INCLUDE, ">:utf8", "$page_file_name.include" ) || EPrints::abort( "Failed to write $page_file_name.include: $!" );
 
 		my $navigation_aids = EPrints::XML::to_string( 
 			render_navigation_aids( $session, $path_values, $view, \@fields, "list" ) );
