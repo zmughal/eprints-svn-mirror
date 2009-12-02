4560bbf493692bcb37dae6569748986c4298c694
tdb01r <tdb01r@9491667e-5006-0410-a446-efbe8990b998>
2009-12-02 12:01:02 +0000

    * 3.2.0-beta-1

---
+++ tags/3.2.0-beta-1/system/perl_lib/EPrints/Plugin/Export/XML.pm
@@ -0,0 +1,102 @@
+package EPrints::Plugin::Export::XML;
+
+use EPrints::Plugin::Export::XMLFile;
+
+@ISA = ( "EPrints::Plugin::Export::XMLFile" );
+
+use strict;
+
+sub new
+{
+	my( $class, %opts ) = @_;
+
+	my( $self ) = $class->SUPER::new( %opts );
+
+	$self->{name} = "EP3 XML";
+	$self->{accept} = [ 'list/*', 'dataobj/*' ];
+	$self->{visible} = "all";
+	$self->{xmlns} = "http://eprints.org/ep2/data/2.0";
+	$self->{qs} = 0.8;
+
+	return $self;
+}
+
+
+
+
+
+sub output_list
+{
+	my( $plugin, %opts ) = @_;
+
+	my $type = $opts{list}->get_dataset->confid;
+	my $toplevel = $type."s";
+	my $namespace = EPrints::XML::namespace( "data", "2" );
+	
+	my $r = [];
+
+	my $part;
+	$part = '<?xml version="1.0" encoding="utf-8" ?>'."\n<$toplevel xmlns=\"$namespace\">\n";
+	if( defined $opts{fh} )
+	{
+		print {$opts{fh}} $part;
+	}
+	else
+	{
+		push @{$r}, $part;
+	}
+
+	$opts{list}->map( sub {
+		my( $session, $dataset, $item ) = @_;
+
+		my $part = $plugin->output_dataobj( $item, %opts );
+		if( defined $opts{fh} )
+		{
+			print {$opts{fh}} $part;
+		}
+		else
+		{
+			push @{$r}, $part;
+		}
+	} );
+
+	$part= "</$toplevel>\n";
+	if( defined $opts{fh} )
+	{
+		print {$opts{fh}} $part;
+	}
+	else
+	{
+		push @{$r}, $part;
+	}
+
+
+	if( defined $opts{fh} )
+	{
+		return;
+	}
+
+	return join( '', @{$r} );
+}
+
+sub output_dataobj
+{
+	my( $plugin, $dataobj ) = @_;
+
+	my $itemtype = $dataobj->get_dataset->confid;
+
+	my $xml = $plugin->xml_dataobj( $dataobj );
+
+	EPrints::XML::tidy( $xml, {}, 1 );
+
+	return "  " . EPrints::XML::to_string( $xml ) . "\n";
+}
+
+sub xml_dataobj
+{
+	my( $plugin, $dataobj ) = @_;
+
+	return $dataobj->to_xml;
+}
+
+1;
---
+++ tags/3.2.0-beta-1/system/perl_lib/EPrints/Search/Condition/InSubject.pm
@@ -0,0 +1,96 @@
+######################################################################
+#
+# EPrints::Search::Condition::InSubject
+#
+######################################################################
+#
+#  __COPYRIGHT__
+#
+# Copyright 2000-2008 University of Southampton. All Rights Reserved.
+# 
+#  __LICENSE__
+#
+######################################################################
+
+=pod
+
+=head1 NAME
+
+B<EPrints::Search::Condition::InSubject> - "InSubject" search condition
+
+=head1 DESCRIPTION
+
+Matches items which are in the subject or a sub-subject
+
+=cut
+
+package EPrints::Search::Condition::InSubject;
+
+use EPrints::Search::Condition::Comparison;
+
+@ISA = qw( EPrints::Search::Condition::Comparison );
+
+use strict;
+
+sub new
+{
+	my( $class, @params ) = @_;
+
+	my $self = {};
+	$self->{op} = "in_subject";
+	$self->{dataset} = shift @params;
+	$self->{field} = shift @params;
+	$self->{params} = \@params;
+
+	return bless $self, $class;
+}
+
+sub joins
+{
+	my( $self, %opts ) = @_;
+
+	my $prefix = $opts{prefix};
+	$prefix = "" if !defined $prefix;
+
+	my $db = $opts{session}->get_database;
+	my $sql_name = $self->{field}->get_sql_name;
+
+	my( $join ) = $self->SUPER::joins( %opts );
+
+	if( defined $join )
+	{
+		return ($join, {
+			type => "inner",
+			table => "subject_ancestors",
+			alias => "${prefix}subject_ancestors",
+			logic => $db->quote_identifier( $join->{alias}, $sql_name )."=".$db->quote_identifier( "${prefix}subject_ancestors", "subjectid" ),
+		});
+	}
+	else
+	{
+		my $main_table = $opts{dataset}->get_sql_table_name;
+		my $alias = $main_table."_".refaddr($self);
+		my $key_field = $opts{dataset}->get_key_field;
+		my $sql = "";
+		$sql = $db->quote_identifier( $main_table )." ".$db->quote_identifier( $alias );
+		$sql .= " INNER JOIN ".$db->quote_identifier( "subject_ancestors" );
+		$sql .= " ON ".$db->quote_identifier( $main_table, $sql_name )."=".$db->quote_identifier( "subject_ancestors", "subjectid" );
+		return {
+			type => "inner",
+			subquery => $sql,
+			key => $key_field->get_sql_name,
+			logic => $db->quote_identifier( $opts{dataset}->get_sql_table_name, $key_field->get_sql_name )."=".$db->quote_identifier( $alias, $key_field->get_sql_name ),
+		};
+	}
+}
+
+sub logic
+{
+	my( $self, %opts ) = @_;
+
+	my $db = $opts{session}->get_database;
+
+	return $db->quote_identifier( "subject_ancestors", "ancestors" )." = ".$db->quote_value( $self->{params}->[0] );
+}
+
+1;
