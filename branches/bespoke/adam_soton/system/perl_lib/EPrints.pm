bed1b390d39b151e54ed9c5a1615ffb9b5e6cbe5
af05v@ecs.soton.ac.uk <af05v@ecs.soton.ac.uk@9491667e-5006-0410-a446-efbe8990b998>
2010-02-11 10:47:16 +0000

    created 3.1.3 branch

---
+++ branches/bespoke/adam_soton/system/perl_lib/EPrints.pm
@@ -0,0 +1,249 @@
+######################################################################
+#
+# EPrints
+#
+######################################################################
+#
+#  __COPYRIGHT__
+#
+# Copyright 2000-2009 University of Southampton. All Rights Reserved.
+# 
+#  __LICENSE__
+#
+######################################################################
+
+package EPrints;
+
+use EPrints::SystemSettings;
+
+use Scalar::Util;
+
+=head1 NAME
+
+B<EPrints> - EPrints Software
+
+=head1 SYNOPSIS
+
+  use EPrints;
+
+  my $session = EPrints::Session->new( 1, "xxx" );
+
+  $session->terminate;
+
+=head1 DESCRIPTION
+
+EPrints is generic repository building software developed by the University of Southampton. It is intended to create a highly configurable web-based repository.
+
+For more information on EPrints see L<http://www.eprints.org/software/>.
+
+=head1 METHODS
+
+=cut
+
+BEGIN {
+	use Carp qw(cluck);
+
+	use EPrints::Platform;
+
+	umask( 0002 );
+
+	if( $ENV{MOD_PERL} )
+	{
+		eval '
+use Apache::DBI; # must be first! 	 	 
+#$Apache::DBI::DEBUG = 3;
+use EPrints::Apache::AnApache;
+use EPrints::Apache::Login;
+use EPrints::Apache::Auth;
+use EPrints::Apache::Rewrite;
+use EPrints::Apache::VLit;
+use EPrints::Apache::Template;
+1;';
+		if( $@ ) { abort( $@ ); }
+	}
+
+	# abort($err) Defined here so modules can abort even at startup
+######################################################################
+=pod
+
+=item EPrints::abort( $msg )
+
+Print an error message and exit. If running under mod_perl then
+print the error as a webpage and exit.
+
+This subroutine is loaded before other modules so that it may be
+used to report errors when initialising modules.
+
+=cut
+######################################################################
+
+	sub abort
+	{
+		my( $errmsg ) = @_;
+
+		my $r;
+		if( $ENV{MOD_PERL} && $EPrints::SystemSettings::loaded)
+		{
+			$r = EPrints::Apache::AnApache::get_request();
+		}
+		if( defined $r )
+		{
+			# If we are running under MOD_PERL
+			# AND this is actually a request, not startup,
+			# then we should print an explanation to the
+			# user in addition to logging to STDERR.
+			my $htmlerrmsg = $errmsg;
+			$htmlerrmsg=~s/&/&amp;/g;
+			$htmlerrmsg=~s/>/&gt;/g;
+			$htmlerrmsg=~s/</&lt;/g;
+			$htmlerrmsg=~s/\n/<br \/>/g;
+			$r->content_type( 'text/html' );
+			EPrints::Apache::AnApache::send_status_line( $r, 500, "EPrints Internal Error" );
+
+			EPrints::Apache::AnApache::send_http_header( $r );
+			print <<END;
+<html>
+  <head>
+    <title>EPrints System Error</title>
+  </head>
+  <body>
+    <h1>EPrints System Error</h1>
+    <p><tt>$htmlerrmsg</tt></p>
+  </body>
+</html>
+END
+		}
+
+		
+		print STDERR <<END;
+	
+------------------------------------------------------------------
+---------------- EPrints System Error ----------------------------
+------------------------------------------------------------------
+$errmsg
+------------------------------------------------------------------
+END
+		$@="";
+		cluck( "EPrints System Error inducing stack dump\n" );
+		exit( 1 );
+	}
+
+=item EPrints::deprecated()
+
+Prints a deprecated warning for the calling sub.
+
+=cut
+
+	sub deprecated
+	{
+		my @c = caller(1);
+		print STDERR "Called deprecated function $c[3] from $c[1] line $c[2]\n";
+	}
+
+=item EPrints::try( CODE_REF )
+
+Attempts to call CODE_REF and if an error occurs calls L</abort> with the error message.
+
+=cut
+
+	sub try
+	{
+		my( $code ) = @_;
+
+		my $r = eval { &$code };
+
+		if( $@ ) { EPrints::abort( $@ ); }
+
+		return $r;
+	}
+}
+
+use EPrints::BackCompatibility;
+use EPrints::XML;
+use EPrints::Utils;
+use EPrints::Time;
+
+use EPrints::Box;
+use EPrints::Config;
+use EPrints::Database;
+use EPrints::DataObj;
+use EPrints::DataObj::Access;
+use EPrints::DataObj::Cachemap;
+use EPrints::DataObj::Document;
+use EPrints::DataObj::EPrint;
+use EPrints::DataObj::History;
+use EPrints::DataObj::Import;
+use EPrints::DataObj::LoginTicket;
+use EPrints::DataObj::Message;
+use EPrints::DataObj::MetaField;
+use EPrints::DataObj::Request;
+use EPrints::DataObj::Subject;
+use EPrints::DataObj::SavedSearch;
+use EPrints::DataObj::User;
+use EPrints::DataSet;
+use EPrints::Email;
+use EPrints::Extras;
+use EPrints::Index;
+use EPrints::Index::Daemon;
+use EPrints::Language;
+use EPrints::Latex;
+use EPrints::List;
+use EPrints::MetaField;
+use EPrints::OpenArchives;
+use EPrints::Paginate;
+use EPrints::Paginate::Columns;
+use EPrints::Probity;
+use EPrints::Repository;
+use EPrints::Search;
+use EPrints::Search::Field;
+use EPrints::Search::Condition;
+use EPrints::CLIProcessor;
+use EPrints::ScreenProcessor;
+use EPrints::Session;
+use EPrints::Script;
+use EPrints::URL;
+use EPrints::Paracite;
+use EPrints::Update::Static;
+use EPrints::Update::Views;
+use EPrints::Update::Abstract;
+use EPrints::Workflow;
+use EPrints::Workflow::Stage;
+use EPrints::Workflow::Processor;
+use EPrints::XML::EPC;
+
+# Load EPrints::Plugin last, because dynamically loaded plugins may have
+# EPrints dependencies
+use EPrints::Plugin;
+
+our $__loaded;
+
+sub import
+{
+	my( $class, @args ) = @_;
+
+	my %opts = map { $_ => 1 } @args;
+
+	# mod_perl will probably be running as root for the main httpd.
+	# The sub processes should run as the same user as the one specified
+	# in $EPrints::SystemSettings
+	# An exception to this is running as root (uid==0) in which case
+	# we can become the required user.
+	if( !$__loaded && !$opts{"no_check_user"} && !$ENV{MOD_PERL} && !$ENV{EPRINTS_NO_CHECK_USER} )
+	{
+		EPrints::Platform::test_uid();
+	}
+
+	$__loaded = 1;
+}
+
+=head1 SEE ALSO
+
+L<EPrints::Session>, L<EPrints::Repository>.
+
+=head1 AUTHOR
+
+Copyright 2000-2009 University of Southampton, UK.
+
+=cut
+
+1;
---
+++ branches/bespoke/adam_soton/system/perl_lib/EPrints/Apache/Auth.pm
@@ -0,0 +1,304 @@
+######################################################################
+#
+# EPrints::Apache::Auth
+#
+######################################################################
+#
+#  __COPYRIGHT__
+#
+# Copyright 2000-2008 University of Southampton. All Rights Reserved.
+# 
+#  __LICENSE__
+#
+######################################################################
+
+
+=pod
+
+=head1 NAME
+
+B<EPrints::Apache::Auth> - Password authentication & authorisation checking 
+for EPrints.
+
+=head1 DESCRIPTION
+
+This module handles the authentication and authorisation of users
+viewing private sections of an EPrints website.
+
+=over 4
+
+=cut
+######################################################################
+
+package EPrints::Apache::Auth;
+
+use strict;
+
+use EPrints::Apache::AnApache; # exports apache constants
+
+#use EPrints::Session;
+#use EPrints::SystemSettings;
+
+
+
+sub authen
+{
+	my( $r ) = @_;
+
+	return OK unless $r->is_initial_req; # only the first internal request
+	
+	my $session = new EPrints::Session(2); # don't open the CGI info
+	
+	if( !defined $session )
+	{
+		return FORBIDDEN;
+	}
+
+	my $rc;
+	if( $session->get_archive->get_conf( "cookie_auth" ) ) 
+	{
+		$rc = auth_cookie( $r, $session );
+	}
+	else
+	{
+		$rc = auth_basic( $r, $session );
+	}
+
+	$session->terminate();
+
+	return $rc;
+}
+
+sub authen_doc
+{
+	my( $r ) = @_;
+
+	my $session = new EPrints::Session(2); # don't open the CGI info
+
+	return FORBIDDEN if( !defined $session );
+
+	my $rvalue = _authen_doc( $r, $session );
+	$session->terminate;
+
+	return $rvalue;
+}
+
+sub _authen_doc
+{
+	my( $r, $session ) = @_;
+
+	my $document = secure_doc_from_url( $r, $session );
+	return NOT_FOUND if( !defined $document );
+
+	my $security = $document->get_value( "security" );
+
+	my $result = $session->get_repository->call( "can_request_view_document", $document, $r );
+
+	return OK if( $result eq "ALLOW" );
+	return FORBIDDEN if( $result eq "DENY" );
+	if( $result ne "USER" )
+	{
+		$session->get_repository->log( "Response from can_request_view_document was '$result'. Only ALLOW, DENY, USER are allowed." );
+		return FORBIDDEN;
+	}
+
+	my $rc;
+	if( $session->get_archive->get_conf( "cookie_auth" ) ) 
+	{
+		$rc = auth_cookie( $r, $session, 1 );
+	}
+	else
+	{
+		$rc = auth_basic( $r, $session );
+	}
+
+	return $rc;
+}
+
+
+
+
+sub auth_cookie
+{
+	my( $r, $session, $redir ) = @_;
+
+	my $user = $session->current_user;
+
+	if( !defined $user ) 
+	{
+		my $login_url = $session->get_url(
+			path => "cgi",
+		) . "/users/login";
+		my $target_url = $session->get_url(
+			host => 1,
+			path => "auto",
+		);
+		$login_url = URI->new( $login_url );
+		$login_url->query_form(
+			target => $target_url
+		);
+		if( $session->get_repository->can_call( 'get_login_url' ) )
+		{
+			$login_url = $session->get_repository->call( 'get_login_url', $session, $target_url );
+			$redir = 1;
+		}
+		if( $redir )
+		{
+			EPrints::Apache::AnApache::send_status_line( $r, 302, "Need to login first" );
+			EPrints::Apache::AnApache::header_out( $r, "Location", $login_url );
+			EPrints::Apache::AnApache::send_http_header( $r );
+			return DONE;
+		}
+
+
+		# bad ticket or no ticket
+		my $av =  $EPrints::SystemSettings::conf->{apache};
+		if( !defined $av || $av eq "1" ) 
+		{
+			$r->set_handlers(PerlResponseHandler =>[ 'EPrints::Apache::Login', 'Apache::Registry' ] );
+		}
+		else # apache 2
+		{
+			$r->set_handlers(PerlResponseHandler =>[ 'EPrints::Apache::Login', 'ModPerl::Registry' ] );
+		}
+
+	}
+
+	return OK;
+}
+
+
+sub auth_basic
+{
+	my( $r, $session ) = @_;
+
+	my( $res, $passwd_sent ) = $r->get_basic_auth_pw;
+	my( $user_sent ) = $r->user;
+
+	if( !defined $user_sent )
+	{
+		return AUTH_REQUIRED;
+	}
+
+	my $area = $r->dir_config( "EPrints_Security_Area" );
+
+	my $user_ds = $session->get_repository->get_dataset( "user" );
+
+	my $user = EPrints::DataObj::User::user_with_username( $session, $user_sent );
+	if( !defined $user )
+	{
+		$r->note_basic_auth_failure;
+		return AUTH_REQUIRED;
+	}
+
+	return $session->valid_login( $user_sent, $passwd_sent );
+}
+
+sub authz
+{
+	my( $r ) = @_;
+
+
+	return OK;
+}
+
+sub authz_doc
+{
+	my( $r ) = @_;
+
+	my $session = new EPrints::Session(2); # don't open the CGI info
+
+	my $document = secure_doc_from_url( $r, $session );
+	if( !defined $document ) 
+	{
+		$session->terminate();
+		return NOT_FOUND;
+	}
+
+	my $request_result = $session->get_repository->call( "can_request_view_document", $document, $r );
+	return OK if( $request_result eq "ALLOW" );
+	return FORBIDDEN if( $request_result eq "DENY" );
+
+	my $security = $document->get_value( "security" );
+
+	my $user = $session->current_user;
+
+	my $result = $document->user_can_view( $user );
+	$session->terminate();
+
+	if( $result )
+	{
+		return OK;
+	}
+	else
+	{
+		return FORBIDDEN;
+	}
+}
+
+######################################################################
+=pod
+
+=item $document = EPrints::Apache::Auth::secure_doc_from_url( $r, $session )
+
+Return the document that the current URL, in the secure documents area
+relates to, if any. Or undef.
+
+=cut
+######################################################################
+
+
+sub secure_doc_from_url
+{
+	my( $r, $session ) = @_;
+
+	# hack to reduce load. We cache the document in the request object.
+	#if( defined $r->{eprint_document} ) { return $r->{eprint_document}; }
+
+	my $repository = $session->{repository};
+	my $uri = $r->uri;
+
+	my $urlpath = $repository->get_conf( "rel_path" );
+
+	$uri =~ s/^$urlpath//;
+
+	my $eprintid;
+	my $pos;
+	if( $uri =~ m#^/(\d+)/(thumbnails/)?(\d+)/# )
+	{
+		$eprintid = $1+0;
+		$pos = $3+0;
+	}
+	else
+	{
+		$repository->log( 
+"Request to ".$r->uri." in secure documents area failed to match REGEXP." );
+		return undef;
+	}
+
+	my $document = EPrints::DataObj::Document::doc_with_eprintid_and_pos( $session, $eprintid, $pos );
+	if( !defined $document ) {
+		$repository->log( 
+"Request to ".$r->uri.": document eprintid=$eprintid pos=$pos not found." );
+		return undef;
+	}
+
+	# cache $document in the request object
+	#$r->{eprint_document} = $document;
+
+
+	return $document;
+}
+
+
+
+
+1;
+
+######################################################################
+=pod
+
+=back
+
+=cut
+
