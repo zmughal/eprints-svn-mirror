2573f1ee5af533dfaaa0ceece241f20ca0f056bd
af05v <af05v@ecs.soton.ac.uk>
2010-04-29 15:43:18 +0000

    moved pdf encryption warning

--- coversheets/system/lib/defaultcfg/cfg.d/eprint_warnings.pl
+++ coversheets/system/lib/defaultcfg/cfg.d/eprint_warnings.pl
@@ -36,6 +36,29 @@ $c->{eprint_warnings} = sub
 		push @problems, $session->html_phrase( "warnings:no_documents" );
 	}
 
+##Coversheet Package
+	foreach my $doc ( @docs )
+	{
+		if ($doc->get_value('format') eq 'application/pdf')
+		{
+
+		##Check the uploaded file isn't encrypted
+			use PDF::API2;
+			my $file_path = $doc->local_path."/".$doc->get_main;
+			if (-e $file_path and $file_path =~ m/\.pdf$/i)
+			{
+				my $pdf = PDF::API2->open( $file_path );
+				if ($pdf->isEncrypted)
+				{
+					my $fieldname = $session->make_element( "span", class=>"ep_problem_field:documents" );
+					push @problems, $session->html_phrase( "validate:encrypted_pdf", fieldname => $fieldname );
+				}
+			}
+		}
+	}
+##End of coversheet addition
+
+
 	my $all_public = 1;
 	foreach my $doc ( @docs )
 	{
