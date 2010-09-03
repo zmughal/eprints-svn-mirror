3d937fdf67f31bbb464472e7e345246b422eb1c0
tdb01r <tdb01r@9491667e-5006-0410-a446-efbe8990b998>
2010-09-03 16:37:33 +0000

    * 3.2.4 Victoria Sponge

---
+++ tags/3.2.4/system/perl_lib/EPrints/Plugin/Screen/Admin/Reload.pm
@@ -0,0 +1,70 @@
+package EPrints::Plugin::Screen::Admin::Reload;
+
+@ISA = ( 'EPrints::Plugin::Screen' );
+
+use strict;
+
+sub new
+{
+	my( $class, %params ) = @_;
+
+	my $self = $class->SUPER::new(%params);
+	
+	$self->{actions} = [qw/ reload_config /]; 
+		
+	$self->{appears} = [
+		{ 
+			place => "admin_actions_config", 
+			position => 1250, 
+			action => "reload_config",
+		},
+	];
+
+	return $self;
+}
+
+sub allow_reload_config
+{
+	my( $self ) = @_;
+
+	return $self->allow( "config/reload" );
+}
+
+sub action_reload_config
+{
+	my( $self ) = @_;
+
+	my $session = $self->{session};
+
+	my( $result, $msg ) = $session->get_repository->test_config;
+
+	if( $result != 0 )
+	{
+		$self->{processor}->add_message( "error",
+			$self->html_phrase( 
+				"reload_bad_config",
+				output=>$self->{session}->make_text( $msg ) ) );
+		$self->{processor}->{screenid} = "Admin";
+		return;
+	}
+
+	my $file = $session->get_repository->get_conf( "variables_path" )."/last_changed.timestamp";
+	unless( open( CHANGEDFILE, ">$file" ) )
+	{
+		$self->{processor}->add_message( "error",
+			$self->html_phrase( "reload_write_failed" ) );
+		$self->{processor}->{screenid} = "Admin";
+		return;
+	}
+	print CHANGEDFILE "This file last poked at: ".EPrints::Time::human_time()."\n";
+	close CHANGEDFILE;
+
+	$self->{processor}->add_message( "message",
+		$self->html_phrase( "reloaded" ) );
+	$self->{processor}->{screenid} = "Admin";
+}	
+
+
+
+
+1;
