69182702659c305f3f91ef6fa3a28647cb951125
tdb01r <tdb01r@9491667e-5006-0410-a446-efbe8990b998>
2011-06-03 09:49:19 +0000

    * Merged [6627]  * Check can_be_viewed so we actually show no_tools if Items is not viewable, otherwise fallback to User::View (profile)

--- system/perl_lib/EPrints/Plugin/Screen/FirstTool.pm
+++ system/perl_lib/EPrints/Plugin/Screen/FirstTool.pm
@@ -1,3 +1,24 @@
+=head1 NAME
+
+EPrints::Plugin::Screen::FirstTool - the first screen to show
+
+=head1 DESCRIPTION
+
+This plugin is the screen shown by the L<EPrints::ScreenProcessor> if there is no 'screen' parameter.
+
+By default this plugin redirects to L<EPrints::Plugin::Screen::Items> or L<EPrints::Plugin::Screen::User::View> for minusers. To override this use:
+
+	$c->{plugins}->{"Screen::FirstTool"}->{params}->{default} = "Screen::First";
+
+Or for a customisable screen supply a callback (which will be passed the FirstTool screen):
+
+	$c->{plugins}->{"Screen::FirstTool"}->{params}->{default} = sub {
+		my( $screen ) = @_;
+
+		return "Items";
+	};
+
+=cut
 
 package EPrints::Plugin::Screen::FirstTool;
 
@@ -7,18 +28,47 @@ use EPrints::Plugin::Screen::EPrint;
 
 use strict;
 
-
-sub from
+sub properties_from
 {
 	my( $self ) = @_;
 
 	my $screenid = $self->param( "default" );
-	$screenid = "Items" if !defined $screenid;
-	my $screen = $self->{session}->plugin( "Screen::$screenid" );
+	my $screen;
+
+	# we will always try to show the custom screen even if it gets an access
+	# denied
+	if( defined $screenid )
+	{
+		if( ref($screenid) eq "CODE" )
+		{
+			$screenid = &$screenid( $self );
+		}
+		if( defined $screenid )
+		{
+			$screen = $self->{session}->plugin( "Screen::$screenid",
+					processor => $self->{processor},
+				);
+		}
+	}
+	else
+	{
+		# the old behaviour was to pick out the first "key_tool" but that isn't
+		# helpful when the first key_tool is now Login (/Logout)
+		for(qw( Items User::View ))
+		{
+			$screenid = $_;
+			$screen = $self->{session}->plugin( "Screen::$screenid",
+					processor => $self->{processor},
+				);
+			next if !defined $screen;
+			undef $screen if !$screen->can_be_viewed;
+			last;
+		}
+	}
 	if( defined $screen )
 	{
 		$self->{processor}->{screenid} = $screenid;
-		$self->SUPER::from;
+		$screen->properties_from;
 	}
 }
 
@@ -26,12 +76,36 @@ sub render
 {
 	my( $self ) = @_;
 
-	my $chunk = $self->{session}->make_doc_fragment;
-
-	$chunk->appendChild( $self->html_phrase( "no_tools" ) );
-
-	return $chunk;
+	return $self->html_phrase( "no_tools" );
 }
 
 1;
+
+
+=head1 COPYRIGHT
+
+=for COPYRIGHT BEGIN
+
+Copyright 2000-2011 University of Southampton.
+
+=for COPYRIGHT END
+
+=for LICENSE BEGIN
+
+This file is part of EPrints L<http://www.eprints.org/>.
+
+EPrints is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+EPrints is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with EPrints.  If not, see L<http://www.gnu.org/licenses/>.
+
+=for LICENSE END
 
