2185298de23d083236b50c41f49c1c518f21120b
tdb01r <tdb01r@9491667e-5006-0410-a446-efbe8990b998>
2011-06-17 14:30:55 +0000

    * 3.2.8

---
+++ tags/3.2.8/3.2/system/perl_lib/EPrints/Search/Condition/OrSubQuery.pm
@@ -0,0 +1,44 @@
+######################################################################
+#
+# EPrints::Search::Condition::SubQuery
+#
+######################################################################
+#
+#  __COPYRIGHT__
+#
+# Copyright 2000-2008 University of Southampton. All Rights Reserved.
+# 
+#  __LICENSE__
+#
+######################################################################
+
+=pod
+
+=head1 NAME
+
+B<EPrints::Search::Condition::SubQuery> - SubQuery
+
+=head1 DESCRIPTION
+
+SubQuery is used internally by the search optimisation to make OR queries on the same table more efficient.
+
+=cut
+
+package EPrints::Search::Condition::OrSubQuery;
+
+use EPrints::Search::Condition::SubQuery;
+
+@ISA = qw( EPrints::Search::Condition::SubQuery );
+
+use strict;
+
+sub logic
+{
+	my( $self, %opts ) = @_;
+
+	my @logic = map { $_->logic( %opts ) } @{$self->{sub_ops}};
+
+	return @logic ? "(".join(" OR ", @logic).")" : ();
+}
+
+1;
