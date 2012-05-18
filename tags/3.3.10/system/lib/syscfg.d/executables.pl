98f1b1c39be813d4c3db04c2376d9dae61d2eb2f
tdb01r <tdb01r@9491667e-5006-0410-a446-efbe8990b998>
2012-05-18 13:15:37 +0000

    * 3.3.10

---
+++ tags/3.3.10/system/lib/syscfg.d/executables.pl
@@ -0,0 +1,48 @@
+# set this to 1 if disk free doesn't work on your system
+$c->{disable_df} = 0 if !defined $EPrints::SystemSettings::conf->{disable_df};
+$c->{executables} ||= {};
+
+# location of executables
+{
+use Config; # for perlpath
+my %executables = (
+	  'convert' => '/usr/bin/convert',
+	  'tar' => '/bin/tar',
+	  'rm' => '/bin/rm',
+	  'dvips' => '/usr/bin/dvips',
+	  'gunzip' => '/bin/gunzip',
+	  'sendmail' => '/usr/sbin/sendmail',
+	  'unzip' => '/usr/bin/unzip',
+	  'elinks' => '/usr/bin/elinks',
+	  'cp' => '/bin/cp',
+	  'latex' => '/usr/bin/latex',
+	  'perl' => $Config{perlpath},
+	  'pdftotext' => '/usr/bin/pdftotext',
+	  'wget' => '/usr/bin/wget',
+	  'antiword' => '/usr/bin/antiword',
+	  'ffmpeg' => '/usr/bin/ffmpeg',
+	  'file' => '/usr/bin/file',
+	  'doc2txt' => "$c->{base_path}/tools/doc2txt",
+	  'unoconv' => '/usr/bin/unoconv',
+	  'txt2refs' => "$c->{base_path}/tools/txt2refs",
+	  'ffprobe' => '/usr/bin/ffprobe',
+	);
+my @paths = EPrints->system->bin_paths;
+EXECUTABLE: while(my( $name, $path ) = each %executables)
+{
+	next if exists $c->{executables}->{$name};
+	if( -x $path )
+	{
+		$c->{executables}->{$name} = $path;
+		next EXECUTABLE;
+	}
+	for(@paths)
+	{
+		if( -x "$_/$name" )
+		{
+			$c->{executables}->{$name} = "$_/$name";
+			next EXECUTABLE;
+		}
+	}
+}
+}
